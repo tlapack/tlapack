e554aa7a454d30a0f6fdb94fc031e963a5a30ed3
Julien Langou <julien.langou@ucdenver.edu>
2022-06-02 15:22:10 -0600

    testing for Lower and Upper now

--- include/lapack/trtri_recursive.hpp
+++ include/lapack/trtri_recursive.hpp
@@ -13,136 +13,144 @@
 #include "lapack/larfg.hpp"
 #include "lapack/larf.hpp"
 
-namespace tlapack {
+namespace tlapack
+{
 
-/** TRTRI computes the inverse of a triangular matrix in-place
- * Input is a triangular matrix, output is its inverse
- * This is the recursive variant
- *
- * @param[in] uplo
- *      - Uplo::Upper: Upper triangle of A is referenced; the strictly lower
- *      triangular part of A is not referenced.
- *      - Uplo::Lower: Lower triangle of A is referenced; the strictly upper
- *      triangular part of A is not referenced.
- * 
- * @param[in,out] A n-by-n matrix.
- *      On entry, the n-by-n triangular matrix to be inverted.
- *      On exit, the inverse.
- * 
- * @param[in] ec Exception handling configuration at runtime.   
- * 
- * @return = 0: successful exit
- * @return > 0: if return value = i, A(i,i) is exactly zero.  The triangular
- *          matrix is singular and its inverse can not be computed.
- * 
- * @todo: implement nx to bail out of recursion before 1-by-1 case
- *
- */
-template <typename matrix_t>
-int trtri_recursive( const Uplo & uplo, matrix_t & C, const ErrorCheck& ec = {}){
+    /** TRTRI computes the inverse of a triangular matrix in-place
+     * Input is a triangular matrix, output is its inverse
+     * This is the recursive variant
+     *
+     * @param[in] uplo
+     *      - Uplo::Upper: Upper triangle of A is referenced; the strictly lower
+     *      triangular part of A is not referenced.
+     *      - Uplo::Lower: Lower triangle of A is referenced; the strictly upper
+     *      triangular part of A is not referenced.
+     *
+     * @param[in,out] A n-by-n matrix.
+     *      On entry, the n-by-n triangular matrix to be inverted.
+     *      On exit, the inverse.
+     *
+     * @param[in] ec Exception handling configuration at runtime.
+     *
+     * @return = 0: successful exit
+     * @return > 0: if return value = i, A(i,i) is exactly zero.  The triangular
+     *          matrix is singular and its inverse can not be computed.
+     *
+     * @todo: implement nx to bail out of recursion before 1-by-1 case
+     *
+     */
+    template <typename matrix_t>
+    int trtri_recursive(const Uplo &uplo, matrix_t &C, const ErrorCheck &ec = {})
+    {
 
-    using T = type_t<matrix_t>;
-    using idx_t = size_type<matrix_t>;
-    using range = std::pair<idx_t, idx_t>;
-    using real_t = real_type<T>;
+        using T = type_t<matrix_t>;
+        using idx_t = size_type<matrix_t>;
+        using range = std::pair<idx_t, idx_t>;
+        using real_t = real_type<T>;
 
-    const idx_t n = nrows(C);
+        const idx_t n = nrows(C);
 
-    const T zero( 0.0 );
+        const T zero(0.0);
 
-    // check arguments
-    tlapack_check_false(    uplo != Uplo::Lower &&
-                            uplo != Uplo::Upper, -1 );
-    tlapack_check_false(    access_denied( uplo, write_policy(C) ), -1 );
-    tlapack_check_false(    nrows(C) != ncols(C), -2 );
+        // check arguments
+        tlapack_check_false(uplo != Uplo::Lower &&
+                                uplo != Uplo::Upper,
+                            -1);
+        tlapack_check_false(access_denied(uplo, write_policy(C)), -1);
+        tlapack_check_false(nrows(C) != ncols(C), -2);
 
-    // Quick return
-    if (n <= 0)
-        return 0;
-
-    idx_t n0 = n / 2;
-
-    if ( n == 1 ){
-
-        if( C(0,0) != zero ) {
-            C(0,0) = real_t(1.) / C(0,0);
+        // Quick return
+        if (n <= 0)
             return 0;
-        }
-        else {
-            tlapack_error_internal( ec, 1,
-                "The leading minor of order 1 is not positive definite,"
-                " and the factorization could not be completed." );
-            return 1;
-        }
 
+        idx_t n0 = n / 2;
 
-    } else {
+        if (n == 1)
+        {
 
-        if (uplo == Uplo::Lower){
-
-            auto C00 = slice( C, range(0,n0), range(0,n0) );
-            auto C10 = slice( C, range(n0,n), range(0,n0) );
-            auto C11 = slice( C, range(n0,n), range(n0,n) );
-
-            trsm(Side::Right, Uplo::Lower, Op::NoTrans, Diag::NonUnit, T(-1), C00, C10);
-            trsm(Side::Left, Uplo::Lower, Op::NoTrans, Diag::NonUnit, T(+1), C11, C10);
-            int info = trtri_recursive( C00, Uplo::Lower);
-            if( info != 0 ) {
-            tlapack_error_internal( ec, info,
-                "A diagonal of entry of triangular matrix is exactly zero." );
-            return info;
-            }
-            info = trtri_recursive( C11, Uplo::Lower);
-            if( info == 0 )
+            if (C(0, 0) != zero)
+            {
+                C(0, 0) = real_t(1.) / C(0, 0);
                 return 0;
-            else {
-                tlapack_error_internal( ec, info + n0,
-                    "A diagonal of entry of triangular matrix is exactly zero." );
-            return info + n0;
             }
-
-            // there are two variants, the code below also works
-
-            // trtri_recursive( C00, Uplo::Lower);
-            // trtri_recursive( C11, Uplo::Lower);        
-            // trmm(Side::Right, Uplo::Lower, Op::NoTrans, Diag::NonUnit, T(-1), C00, C10);
-            // trmm(Side::Left, Uplo::Lower, Op::NoTrans, Diag::NonUnit, T(+1), C11, C10);
-
-        } else {
-
-            auto C00 = slice( C, range(0,n0), range(0,n0) );
-            auto C01 = slice( C, range(0,n0), range(n0,n) );
-            auto C11 = slice( C, range(n0,n), range(n0,n) );
-
-            trsm(Side::Left, Uplo::Upper, Op::NoTrans, Diag::NonUnit, T(-1), C00, C01);
-            trsm(Side::Right, Uplo::Upper, Op::NoTrans, Diag::NonUnit, T(+1), C11, C01);
-            int info = trtri_recursive( C00, Uplo::Upper);
-            if( info != 0 ) {
-            tlapack_error_internal( ec, info,
-                "A diagonal of entry of triangular matrix is exactly zero." );
-            return info;
+            else
+            {
+                tlapack_error_internal(ec, 1,
+                                       "A diagonal of entry of triangular matrix is exactly zero.");
+                return 1;
             }
-            info = trtri_recursive( C11, Uplo::Upper);
-            if( info == 0 )
-                return 0;
-            else {
-                tlapack_error_internal( ec, info + n0,
-                    "A diagonal of entry of triangular matrix is exactly zero." );
-            return info + n0;
+        }
+        else
+        {
+
+            if (uplo == Uplo::Lower)
+            {
+
+                auto C00 = slice(C, range(0, n0), range(0, n0));
+                auto C10 = slice(C, range(n0, n), range(0, n0));
+                auto C11 = slice(C, range(n0, n), range(n0, n));
+
+                trsm(Side::Right, Uplo::Lower, Op::NoTrans, Diag::NonUnit, T(-1), C00, C10);
+                trsm(Side::Left, Uplo::Lower, Op::NoTrans, Diag::NonUnit, T(+1), C11, C10);
+                int info = trtri_recursive(Uplo::Lower, C00, ec);
+                if (info != 0)
+                {
+                    tlapack_error_internal(ec, info,
+                                           "A diagonal of entry of triangular matrix is exactly zero.");
+                    return info;
+                }
+                info = trtri_recursive(Uplo::Lower, C11, ec);
+                if (info == 0)
+                    return 0;
+                else
+                {
+                    tlapack_error_internal(ec, info + n0,
+                                           "A diagonal of entry of triangular matrix is exactly zero.");
+                    return info + n0;
+                }
+
+                // there are two variants, the code below also works
+
+                // trtri_recursive( Uplo::Lower, C00);
+                // trtri_recursive( Uplo::Lower, C11);
+                // trmm(Side::Right, Uplo::Lower, Op::NoTrans, Diag::NonUnit, T(-1), C00, C10);
+                // trmm(Side::Left, Uplo::Lower, Op::NoTrans, Diag::NonUnit, T(+1), C11, C10);
             }
+            else
+            {
 
+                auto C00 = slice(C, range(0, n0), range(0, n0));
+                auto C01 = slice(C, range(0, n0), range(n0, n));
+                auto C11 = slice(C, range(n0, n), range(n0, n));
 
-            // there are two variants, the code below also works
+                trsm(Side::Left, Uplo::Upper, Op::NoTrans, Diag::NonUnit, T(-1), C00, C01);
+                trsm(Side::Right, Uplo::Upper, Op::NoTrans, Diag::NonUnit, T(+1), C11, C01);
+                int info = trtri_recursive(Uplo::Upper, C00, ec);
+                if (info != 0)
+                {
+                    tlapack_error_internal(ec, info,
+                                           "A diagonal of entry of triangular matrix is exactly zero.");
+                    return info;
+                }
+                info = trtri_recursive(Uplo::Upper, C11, ec);
+                if (info == 0)
+                    return 0;
+                else
+                {
+                    tlapack_error_internal(ec, info + n0,
+                                           "A diagonal of entry of triangular matrix is exactly zero.");
+                    return info + n0;
+                }
 
-            // trtri_recursive( C00, Uplo::Upper);
-            // trtri_recursive( C11, Uplo::Upper);        
-            // trmm(Side::Left, Uplo::Upper, Op::NoTrans, Diag::NonUnit, T(-1), C00, C01);
-            // trmm(Side::Right, Uplo::Upper, Op::NoTrans, Diag::NonUnit, T(+1), C11, C01);
+                // there are two variants, the code below also works
 
+                // trtri_recursive( C00, Uplo::Upper);
+                // trtri_recursive( C11, Uplo::Upper);
+                // trmm(Side::Left, Uplo::Upper, Op::NoTrans, Diag::NonUnit, T(-1), C00, C01);
+                // trmm(Side::Right, Uplo::Upper, Op::NoTrans, Diag::NonUnit, T(+1), C11, C01);
+            }
         }
     }
-    
-}
 
 } // lapack
 
--- test/src/test_trtri.cpp
+++ test/src/test_trtri.cpp
@@ -1,5 +1,5 @@
-/// @file test_schur_swap.cpp
-/// @brief Test 1x1 and 2x2 sylvester solver
+/// @file test_trtri.cpp
+/// @brief Test TRTRI
 //
 // Copyright (c) 2022, University of Colorado Denver. All rights reserved.
 //
@@ -15,7 +15,7 @@
 
 using namespace tlapack;
 
-TEMPLATE_LIST_TEST_CASE("move of eigenvalue block gives correct results", "[eigenvalues]", types_to_test)
+TEMPLATE_LIST_TEST_CASE("TRTRI is stable", "[trtri]", types_to_test)
 {
     srand(1);
 
@@ -24,88 +24,67 @@ TEMPLATE_LIST_TEST_CASE("move of eigenvalue block gives correct results", "[eige
     using idx_t = size_type<matrix_t>;
     typedef real_type<T> real_t;
 
-    const T zero(0);
-    const T one(1);
-    idx_t n = 10;
-
-    idx_t n1, n2, ifst, ilst;
-    ifst = GENERATE(0, 2, 6, 9);
-    ilst = GENERATE(0, 2, 6, 9);
-
-    if (is_complex<T>::value)
-    {
-        n1 = 1;
-        n2 = 1;
-    }
-    else
-    {
-        n1 = GENERATE(1, 2);
-        n2 = GENERATE(1, 2);
-    }
-
-    // ifst and ilst point to the same block, n1 must be equal to n2 for
-    // the test to make sense.
-    if (ifst == ilst and n1 != n2)
-        n2 = n1;
+    Uplo uplo = GENERATE(Uplo::Lower, Uplo::Upper);
+    idx_t n = GENERATE(1, 2, 6, 9);
 
     const real_t eps = uroundoff<real_t>();
     const real_t tol = 1.0e2 * n * eps;
 
     std::unique_ptr<T[]> A_(new T[n * n]);
-    std::unique_ptr<T[]> Q_(new T[n * n]);
-    std::unique_ptr<T[]> A_copy_(new T[n * n]);
+    std::unique_ptr<T[]> C_(new T[n * n]);
 
     auto A = legacyMatrix<T, layout<matrix_t>>(n, n, &A_[0], n);
-    auto Q = legacyMatrix<T, layout<matrix_t>>(n, n, &Q_[0], n);
-    auto A_copy = legacyMatrix<T, layout<matrix_t>>(n, n, &A_copy_[0], n);
+    auto C = legacyMatrix<T, layout<matrix_t>>(n, n, &C_[0], n);
 
     // Generate random matrix in Schur form
     for (idx_t j = 0; j < n; ++j)
+    {
         for (idx_t i = 0; i < n; ++i)
             A(i, j) = rand_helper<T>();
-
-    for (idx_t j = 0; j < n; ++j)
-        for (idx_t i = j + 1; i < n; ++i)
-            A(i, j) = zero;
-
-    if (n1 == 2)
-    {
-        if (ifst < n - 1)
-            A(ifst + 1, ifst) = rand_helper<T>();
-        else
-            A(ifst, ifst - 1) = rand_helper<T>();
-    }
-    if (n2 == 2)
-    {
-        if (ilst < n - 1)
-            A(ilst + 1, ilst) = rand_helper<T>();
-        else
-            A(ilst, ilst - 1) = rand_helper<T>();
+        A(j, j) += tlapack::make_scalar<T>(n, 0);
     }
 
-    if( !is_complex<T>::value ){
-        // Put a 2x2 block in the middle
-        A(5, 4) = rand_helper<T>();
-    }
+    lacpy(uplo, A, C);
 
-    lacpy(Uplo::General, A, A_copy);
-    laset(Uplo::General, zero, one, Q);
-
-    DYNAMIC_SECTION("ifst = " << ifst << " n1 = " << n1 << " ilst = " << ilst << " n2 =" << n2)
+    DYNAMIC_SECTION("n = " << n)
     {
-        schur_move(true, A, Q, ifst, ilst);
+        trtri_recursive(uplo, C);
+
         // Calculate residuals
 
-        std::unique_ptr<T[]> _res(new T[n * n]);
-        std::unique_ptr<T[]> _work(new T[n * n]);
+        if (uplo == Uplo::Lower)
+        {
+            for (idx_t j = 0; j < n; j++)
+                for (idx_t i = 0; i < j; i++)
+                    C(i, j) = T(0);
+        }
+        else
+        {
+            for (idx_t i = 0; i < n; i++)
+                for (idx_t j = 0; j < i; j++)
+                    C(i, j) = T(0);
+        }
 
-        auto res = legacyMatrix<T, layout<matrix_t>>(n, n, &_res[0], n);
-        auto work = legacyMatrix<T, layout<matrix_t>>(n, n, &_work[0], n);
-        auto orth_res_norm = check_orthogonality(Q, res);
-        CHECK(orth_res_norm <= tol);
+        // TRMM with X starting as the inverse of C and leaving as the identity. This checks that the inverse is correct.
+        // Note: it would be nice to have a ``upper * upper`` MM function to do this
+        trmm(Side::Left, uplo, Op::NoTrans, Diag::NonUnit, T(1), A, C);
 
-        auto normA = tlapack::lange(tlapack::frob_norm, A);
-        auto simil_res_norm = check_similarity_transform(A_copy, Q, A, res, work);
-        CHECK(simil_res_norm <= tol * normA);
+        for (idx_t i = 0; i < n; ++i)
+            C(i, i) = C(i, i) - T(1);
+
+        real_t normres = lantr(max_norm, uplo, Diag::NonUnit, C) / (lantr(max_norm, uplo, Diag::NonUnit, A));
+        CHECK(normres <= tol);
+
+        // std::unique_ptr<T[]> _res(new T[n * n]);
+        // std::unique_ptr<T[]> _work(new T[n * n]);
+
+        // auto res = legacyMatrix<T, layout<matrix_t>>(n, n, &_res[0], n);
+        // auto work = legacyMatrix<T, layout<matrix_t>>(n, n, &_work[0], n);
+        // auto orth_res_norm = check_orthogonality(Q, res);
+        // CHECK(orth_res_norm <= tol);
+
+        // auto normA = lange(frob_norm, A);
+        // auto simil_res_norm = check_similarity_transform(A_copy, Q, A, res, work);
+        // CHECK(simil_res_norm <= tol * normA);
     }
 }
