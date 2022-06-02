e6a83a6ea3ce5d55868600b83e59073abb0b3ef8
ntebeje <ntebe@DESKTOP-TOQ1319>
2022-06-02 16:00:10 -0600

    Updated larft recursive that works properly for any m and n sizes

--- include/lapack/larft_recursive.h
+++ include/lapack/larft_recursive.h
@@ -31,22 +31,24 @@ int larft_recursive(matrix_t &A, vector_t tau, matrix_t &TTT )
     const idx_t m = nrows(A);
     const idx_t n = ncols(A);
 
-    if( n == 1){
+    const idx_t k = std::min(m,n);
+
+    if( k == 1){
         TTT(0,0) = tau[0];
     } else {
 // assigning the blocks 
-    idx_t n0 = n / 2;
+    idx_t n0 = k / 2;
 
     auto Av0 = tlapack::slice( A, range(0,m), range(0,n0) );
     auto Av1 = tlapack::slice( A, range(n0,m), range(n0,n) );
     auto T00 = tlapack::slice( TTT, range(0,n0), range(0,n0) );
-    auto T11 = tlapack::slice( TTT, range(n0,n), range(n0,n) );
-    auto T01 = tlapack::slice( TTT, range(0,n0), range(n0,n) );
+    auto T11 = tlapack::slice( TTT, range(n0,k), range(n0,k) );
+    auto T01 = tlapack::slice( TTT, range(0,n0), range(n0,k) );
 
 
     // slicing tau (one dimention)
     auto tau0 = tlapack::slice( tau, range(0,n0) );
-    auto tau1 = tlapack::slice( tau, range(n0,n) );
+    auto tau1 = tlapack::slice( tau, range(n0,k) );
 
     // calling the larft fun to compute T
     // tlapack::larft(tlapack::Direction::Forward, tlapack::columnwise_storage, Av0, tau0, T00);
@@ -55,26 +57,29 @@ int larft_recursive(matrix_t &A, vector_t tau, matrix_t &TTT )
     larft_recursive( Av0, tau0, T00);
     larft_recursive( Av1, tau1, T11);
 
-    // Step 1
-    auto A10 = tlapack::slice(A, range(n0, n), range(0, n0));
+    // Step 1 ---- (V10)^H -> T01
+    auto A10 = tlapack::slice(A, range(n0, k), range(0, n0));
     for (idx_t i = 0; i < n0; ++i) {
-        for (idx_t j = 0; j < n-n0; ++j)
+        for (idx_t j = 0; j < k-n0; ++j)
             T01(i,j) = conj(A10(j,i));
     }
 
-    // step 2
-    auto A11 = tlapack::slice(A, range(n0, n), range(n0, n));
+    // step 2--- T01 * V11 -> T01
+    auto A11 = tlapack::slice(A, range(n0, k), range(n0, k));
     tlapack::trmm(tlapack::Side::Right, tlapack::Uplo::Lower, tlapack::Op::NoTrans, tlapack::Diag::Unit, real_t(1), A11, T01 );
 
-    //step 3
-    auto A20 = tlapack::slice(A, range(n, m), range(0, n0));
-    auto A21 = tlapack::slice(A, range(n, m), range(n0, n));
-    tlapack::gemm( tlapack:: Op::ConjTrans, tlapack:: Op::NoTrans, T(1), A20, A21, T(1), T01 );
+    //step 3 --- [T01 + ((V20)^H * V21)] -. T01
+    if(m > n)
+   {
+        auto A20 = tlapack::slice(A, range(n, m), range(0, n0));
+        auto A21 = tlapack::slice(A, range(n, m), range(n0, n));
+        tlapack::gemm( tlapack:: Op::ConjTrans, tlapack:: Op::NoTrans, T(1), A20, A21, T(1), T01 );
+   }
 
-    //step 4
+    //step 4 --- (- T01) * T11 -> T01
     tlapack::trmm(tlapack::Side::Right, tlapack::Uplo::Upper, tlapack::Op::NoTrans, tlapack::Diag::NonUnit, real_t(-1), T11, T01 );
 
-    //step 5
+    //step 5 --T00 * T01  -> T01
     tlapack::trmm(tlapack::Side::Left, tlapack::Uplo::Upper, tlapack::Op::NoTrans, tlapack::Diag::NonUnit, real_t(1), T00, T01 );
     }
     return 0;
