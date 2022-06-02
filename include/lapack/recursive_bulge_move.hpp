75ad51aa60d4cb74c1fa20f6c3fd3f9dfd432d5e
Thijs Steel <steelthijs@gmail.com>
2022-06-02 11:54:36 -0600

    make and test nonrecursive bulge move

--- include/lapack/recursive_bulge_move.hpp
+++ include/lapack/recursive_bulge_move.hpp
@@ -42,7 +42,7 @@ namespace tlapack
      * @param[in] s  complex vector.
      *      Vector containing the shifts to be used during the sweep
      *
-     * @param[out] Q  n-2 by n-2 matrix.
+     * @param[out] Q  n-1 by n-1 matrix.
      *      The orthogonal matrix Q
      *
      * @param[in,out] V    3 by size(s)/2 matrix.
@@ -64,23 +64,20 @@ namespace tlapack
 
         const T one(1);
         const T zero(0);
-        const real_t rone(1);
-        const real_t rzero(0);
         const idx_t n = ncols(A);
         const real_t eps = ulp<real_t>();
         const real_t small_num = safe_min<real_t>() * ((real_t)n / eps);
         const idx_t ns = size(s);
         const idx_t n_bulges = ns / 2;
-        const idx_t nq = ncols(Q);
 
         // Number of positions to move the bulges
-        const idx_t np = n - 2 - ns;
+        const idx_t np = n - 1 - ns;
 
         laset(Uplo::General, zero, one, Q);
 
         for (idx_t ip = 0; ip < np; ++ip)
         {
-            for (idx_t i_bulge = 0; i_bulge < 1; ++i_bulge)
+            for (idx_t i_bulge = 0; i_bulge < n_bulges; ++i_bulge)
             {
                 idx_t i_pos = 1 + ip + 2 * (n_bulges - i_bulge - 1);
                 auto v = col(V, i_bulge);
@@ -159,7 +156,7 @@ namespace tlapack
             // }
 
             // Delayed update from the left
-            for (idx_t i_bulge = 0; i_bulge < 1; ++i_bulge)
+            for (idx_t i_bulge = 0; i_bulge < n_bulges; ++i_bulge)
             {
                 idx_t i_pos = 1 + ip + 2 * (n_bulges - i_bulge - 1);
                 auto v = col(V, i_bulge);
@@ -176,7 +173,7 @@ namespace tlapack
             }
 
             // Accumulate the reflectors into U
-            for (idx_t i_bulge = 0; i_bulge < 1; ++i_bulge)
+            for (idx_t i_bulge = 0; i_bulge < n_bulges; ++i_bulge)
             {
                 idx_t i_pos = ip + 2 * (n_bulges - i_bulge - 1);
                 auto v = col(V, i_bulge);
@@ -191,6 +188,372 @@ namespace tlapack
                     Q(j, i_pos) = Q(j, i_pos) - sum * t0;
                     Q(j, i_pos + 1) = Q(j, i_pos + 1) - sum * t1;
                     Q(j, i_pos + 2) = Q(j, i_pos + 2) - sum * t2;
+                }
+            }
+        }
+
+        return;
+    }
+
+    /** introduce_bulges introduces bulges into the matrix A and moves them down just enough to
+     *  make room for the other bulges.
+     *
+     * @param[in,out] A  n by n matrix.
+     *      Hessenberg matrix with bulges present in the first size(s) positions.
+     *
+     * @param[in] s  complex vector.
+     *      Vector containing the shifts to be used during the sweep
+     *
+     * @param[out] Q  n-1 by n-1 matrix.
+     *      The orthogonal matrix Q
+     *
+     * @param[out] V    3 by size(s)/2 matrix.
+     *      Matrix containing delayed reflectors.
+     *
+     * @ingroup geev
+     */
+    template <
+        class matrix_t,
+        class vector_t,
+        enable_if_t<is_complex<type_t<vector_t>>::value, bool> = true>
+    void introduce_bulges(matrix_t &A, vector_t &s, matrix_t &Q, matrix_t &V)
+    {
+
+        using T = type_t<matrix_t>;
+        using real_t = real_type<T>;
+        using idx_t = size_type<matrix_t>;
+        using pair = std::pair<idx_t, idx_t>;
+
+        const T one(1);
+        const T zero(0);
+        const idx_t n = ncols(A);
+        const real_t eps = ulp<real_t>();
+        const real_t small_num = safe_min<real_t>() * ((real_t)n / eps);
+        const idx_t ns = size(s);
+        const idx_t n_bulges = ns / 2;
+
+        laset(Uplo::General, zero, one, Q);
+
+        for (idx_t i_pos_last = 0; i_pos_last < ns - 1; ++i_pos_last)
+        {
+            // The number of bulges that are in the pencil
+            idx_t n_active_bulges = min(n_bulges, (i_pos_last / 2) + 1);
+            for (idx_t i_bulge = 0; i_bulge < n_active_bulges; ++i_bulge)
+            {
+                idx_t i_pos = i_pos_last - 2 * i_bulge;
+                auto v = col(V, i_bulge);
+                if (i_pos == 0)
+                {
+                    // Introduce bulge
+                    T tau;
+                    auto H = slice(A, pair{0, 3}, pair{0, 3});
+                    lahqr_shiftcolumn(H, v, s[2 * i_bulge], s[2 * i_bulge + 1]);
+                    larfg(v, tau);
+                    v[0] = tau;
+                }
+                else
+                {
+                    // Chase bulge down
+                    auto H = slice(A, pair{i_pos - 1, i_pos + 3}, pair{i_pos - 1, i_pos + 3});
+                    move_bulge(H, v, s[2 * i_bulge], s[2 * i_bulge + 1]);
+                }
+
+                // Apply the reflector we just calculated from the right
+                // We leave the last row for later (it interferes with the optimally packed bulges)
+                for (idx_t j = 0; j < i_pos + 3; ++j)
+                {
+                    auto sum = A(j, i_pos) + v[1] * A(j, i_pos + 1) + v[2] * A(j, i_pos + 2);
+                    A(j, i_pos) = A(j, i_pos) - sum * v[0];
+                    A(j, i_pos + 1) = A(j, i_pos + 1) - sum * v[0] * conj(v[1]);
+                    A(j, i_pos + 2) = A(j, i_pos + 2) - sum * v[0] * conj(v[2]);
+                }
+
+                // Apply the reflector we just calculated from the left
+                // We only update a single column, the rest is updated later
+                auto sum = A(i_pos, i_pos) + conj(v[1]) * A(i_pos + 1, i_pos) + conj(v[2]) * A(i_pos + 2, i_pos);
+                A(i_pos, i_pos) = A(i_pos, i_pos) - sum * conj(v[0]);
+                A(i_pos + 1, i_pos) = A(i_pos + 1, i_pos) - sum * conj(v[0]) * v[1];
+                A(i_pos + 2, i_pos) = A(i_pos + 2, i_pos) - sum * conj(v[0]) * v[2];
+
+                // Test for deflation.
+                if (i_pos > 0)
+                {
+                    if (A(i_pos, i_pos - 1) != zero)
+                    {
+                        auto tst1 = abs1(A(i_pos - 1, i_pos - 1)) + abs1(A(i_pos, i_pos));
+                        if (tst1 == zero)
+                        {
+                            if (i_pos > 1)
+                                tst1 += abs1(A(i_pos - 1, i_pos - 2));
+                            if (i_pos > 2)
+                                tst1 += abs1(A(i_pos - 1, i_pos - 3));
+                            if (i_pos > 3)
+                                tst1 += abs1(A(i_pos - 1, i_pos - 4));
+                            if (i_pos < n - 1)
+                                tst1 += abs1(A(i_pos + 1, i_pos));
+                            if (i_pos < n - 2)
+                                tst1 += abs1(A(i_pos + 2, i_pos));
+                            if (i_pos < n - 3)
+                                tst1 += abs1(A(i_pos + 3, i_pos));
+                        }
+                        if (abs1(A(i_pos, i_pos - 1)) < std::max(small_num, eps * tst1))
+                        {
+                            auto ab = std::max(abs1(A(i_pos, i_pos - 1)), abs1(A(i_pos - 1, i_pos)));
+                            auto ba = std::min(abs1(A(i_pos, i_pos - 1)), abs1(A(i_pos - 1, i_pos)));
+                            auto aa = std::max(abs1(A(i_pos, i_pos)), abs1(A(i_pos, i_pos) - A(i_pos - 1, i_pos - 1)));
+                            auto bb = std::min(abs1(A(i_pos, i_pos)), abs1(A(i_pos, i_pos) - A(i_pos - 1, i_pos - 1)));
+                            auto s = aa + ab;
+                            if (ba * (ab / s) <= std::max(small_num, eps * (bb * (aa / s))))
+                            {
+                                A(i_pos, i_pos - 1) = zero;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // The following code performs the delayed update from the left
+            // it is optimized for column oriented matrices, but the increased complexity
+            // likely causes slower code
+            // for (idx_t j = ilo; j < istop_m; ++j)
+            // {
+            //     idx_t i_bulge_start = (i_pos_last + 2 > j) ? (i_pos_last + 2 - j) / 2 : 0;
+            //     for (idx_t i_bulge = i_bulge_start; i_bulge < n_active_bulges; ++i_bulge)
+            //     {
+            //         idx_t i_pos = i_pos_last - 2 * i_bulge;
+            //         auto v = col(V, i_bulge);
+            //         auto sum = A(i_pos, j) + conj(v[1]) * A(i_pos + 1, j) + conj(v[2]) * A(i_pos + 2, j);
+            //         A(i_pos, j) = A(i_pos, j) - sum * conj(v[0]);
+            //         A(i_pos + 1, j) = A(i_pos + 1, j) - sum * conj(v[0]) * v[1];
+            //         A(i_pos + 2, j) = A(i_pos + 2, j) - sum * conj(v[0]) * v[2];
+            //     }
+            // }
+
+            // Delayed update from the left
+            for (idx_t i_bulge = 0; i_bulge < n_active_bulges; ++i_bulge)
+            {
+                idx_t i_pos = i_pos_last - 2 * i_bulge;
+                auto v = col(V, i_bulge);
+                for (idx_t j = i_pos + 1; j < n; ++j)
+                {
+                    auto sum = A(i_pos, j) + conj(v[1]) * A(i_pos + 1, j) + conj(v[2]) * A(i_pos + 2, j);
+                    A(i_pos, j) = A(i_pos, j) - sum * conj(v[0]);
+                    A(i_pos + 1, j) = A(i_pos + 1, j) - sum * conj(v[0]) * v[1];
+                    A(i_pos + 2, j) = A(i_pos + 2, j) - sum * conj(v[0]) * v[2];
+                }
+            }
+
+            // Accumulate the reflectors into Q
+            for (idx_t i_bulge = 0; i_bulge < n_active_bulges; ++i_bulge)
+            {
+                idx_t i_pos = i_pos_last - 2 * i_bulge;
+                auto v = col(V, i_bulge);
+                idx_t i1 = 0;
+                idx_t i2 = min(nrows(Q), 2 * i_pos_last + 3);
+                for (idx_t j = i1; j < i2; ++j)
+                {
+                    auto sum = Q(j, i_pos) + v[1] * Q(j, i_pos + 1) + v[2] * Q(j, i_pos + 2);
+                    Q(j, i_pos) = Q(j, i_pos) - sum * v[0];
+                    Q(j, i_pos + 1) = Q(j, i_pos + 1) - sum * v[0] * conj(v[1]);
+                    Q(j, i_pos + 2) = Q(j, i_pos + 2) - sum * v[0] * conj(v[2]);
+                }
+            }
+        }
+
+        return;
+    }
+
+    /** remove bulges removes bulges from the matrix A
+     *
+     * @param[in,out] A  n by n matrix.
+     *      Hessenberg matrix with bulges present in the last size(s) positions.
+     *
+     * @param[in] s  complex vector.
+     *      Vector containing the shifts to be used during the sweep
+     *
+     * @param[out] Q  n-1 by n-1 matrix.
+     *      The orthogonal matrix Q
+     *
+     * @param[out] V    3 by size(s)/2 matrix.
+     *      Matrix containing delayed reflectors.
+     *
+     * @ingroup geev
+     */
+    template <
+        class matrix_t,
+        class vector_t,
+        enable_if_t<is_complex<type_t<vector_t>>::value, bool> = true>
+    void remove_bulges(matrix_t &A, vector_t &s, matrix_t &Q, matrix_t &V)
+    {
+
+        using T = type_t<matrix_t>;
+        using real_t = real_type<T>;
+        using idx_t = size_type<matrix_t>;
+        using pair = std::pair<idx_t, idx_t>;
+
+        const T one(1);
+        const T zero(0);
+        const idx_t n = ncols(A);
+        const real_t eps = ulp<real_t>();
+        const real_t small_num = safe_min<real_t>() * ((real_t)n / eps);
+        const idx_t ns = size(s);
+        const idx_t n_bulges = ns / 2;
+
+        laset(Uplo::General, zero, one, Q);
+
+        for (idx_t i_pos_last = n - 2; i_pos_last < n + ns; ++i_pos_last)
+        {
+            idx_t i_bulge_start = (i_pos_last + 3 > n) ? (i_pos_last + 3 - n) / 2 : 0;
+            for (idx_t i_bulge = i_bulge_start; i_bulge < n_bulges; ++i_bulge)
+            {
+                idx_t i_pos = i_pos_last - 2 * i_bulge;
+                if (i_pos == n - 2)
+                {
+                    // Special case, the bulge is at the bottom, needs a smaller reflector (order 2)
+                    auto v = slice(V, pair{0, 2}, i_bulge);
+                    auto h = slice(A, pair{i_pos, i_pos + 2}, i_pos - 1);
+                    larfg(h, v[0]);
+                    v[1] = h[1];
+                    h[1] = zero;
+
+                    auto t1 = conj(v[0]);
+                    auto v2 = v[1];
+                    auto t2 = t1 * v2;
+                    // Apply the reflector we just calculated from the right
+                    for (idx_t j = 0; j < i_pos + 2; ++j)
+                    {
+                        auto sum = A(j, i_pos) + v2 * A(j, i_pos + 1);
+                        A(j, i_pos) = A(j, i_pos) - sum * conj(t1);
+                        A(j, i_pos + 1) = A(j, i_pos + 1) - sum * conj(t2);
+                    }
+                    // Apply the reflector we just calculated from the left
+                    for (idx_t j = i_pos; j < n; ++j)
+                    {
+                        auto sum = A(i_pos, j) + conj(v2) * A(i_pos + 1, j);
+                        A(i_pos, j) = A(i_pos, j) - sum * t1;
+                        A(i_pos + 1, j) = A(i_pos + 1, j) - sum * t2;
+                    }
+                    // Accumulate the reflector into U
+                    // The loop bounds should be changed to reflect the fact that U2 starts off as diagonal
+                    for (idx_t j = 0; j < nrows(Q); ++j)
+                    {
+                        auto sum = Q(j, i_pos - 1) + v2 * Q(j, i_pos);
+                        Q(j, i_pos - 1) = Q(j, i_pos - 1) - sum * conj(t1);
+                        Q(j, i_pos) = Q(j, i_pos) - sum * conj(t2);
+                    }
+                }
+                else
+                {
+                    auto v = col(V, i_bulge);
+                    auto H = slice(A, pair{i_pos - 1, i_pos + 3}, pair{i_pos - 1, i_pos + 3});
+                    move_bulge(H, v, s[2 * i_bulge], s[2 * i_bulge + 1]);
+
+                    auto t1 = conj(v[0]);
+                    auto v2 = v[1];
+                    auto t2 = t1 * v2;
+                    auto v3 = v[2];
+                    auto t3 = t1 * v[2];
+                    // Apply the reflector we just calculated from the right (but leave the last row for later)
+                    for (idx_t j = 0; j < i_pos + 3; ++j)
+                    {
+                        auto sum = A(j, i_pos) + v2 * A(j, i_pos + 1) + v3 * A(j, i_pos + 2);
+                        A(j, i_pos) = A(j, i_pos) - sum * conj(t1);
+                        A(j, i_pos + 1) = A(j, i_pos + 1) - sum * conj(t2);
+                        A(j, i_pos + 2) = A(j, i_pos + 2) - sum * conj(t3);
+                    }
+
+                    // Apply the reflector we just calculated from the left
+                    // We only update a single column, the rest is updated later
+                    auto sum = A(i_pos, i_pos) + conj(v[1]) * A(i_pos + 1, i_pos) + conj(v[2]) * A(i_pos + 2, i_pos);
+                    A(i_pos, i_pos) = A(i_pos, i_pos) - sum * conj(v[0]);
+                    A(i_pos + 1, i_pos) = A(i_pos + 1, i_pos) - sum * conj(v[0]) * v[1];
+                    A(i_pos + 2, i_pos) = A(i_pos + 2, i_pos) - sum * conj(v[0]) * v[2];
+
+                    // Test for deflation.
+                    if (i_pos > 0)
+                    {
+                        if (A(i_pos, i_pos - 1) != zero)
+                        {
+                            auto tst1 = abs1(A(i_pos - 1, i_pos - 1)) + abs1(A(i_pos, i_pos));
+                            if (tst1 == zero)
+                            {
+                                if (i_pos > 1)
+                                    tst1 += abs1(A(i_pos - 1, i_pos - 2));
+                                if (i_pos > 2)
+                                    tst1 += abs1(A(i_pos - 1, i_pos - 3));
+                                if (i_pos > 3)
+                                    tst1 += abs1(A(i_pos - 1, i_pos - 4));
+                                if (i_pos < n - 1)
+                                    tst1 += abs1(A(i_pos + 1, i_pos));
+                                if (i_pos < n - 2)
+                                    tst1 += abs1(A(i_pos + 2, i_pos));
+                                if (i_pos < n - 3)
+                                    tst1 += abs1(A(i_pos + 3, i_pos));
+                            }
+                            if (abs1(A(i_pos, i_pos - 1)) < max(small_num, eps * tst1))
+                            {
+                                auto ab = max(abs1(A(i_pos, i_pos - 1)), abs1(A(i_pos - 1, i_pos)));
+                                auto ba = min(abs1(A(i_pos, i_pos - 1)), abs1(A(i_pos - 1, i_pos)));
+                                auto aa = max(abs1(A(i_pos, i_pos)), abs1(A(i_pos, i_pos) - A(i_pos - 1, i_pos - 1)));
+                                auto bb = min(abs1(A(i_pos, i_pos)), abs1(A(i_pos, i_pos) - A(i_pos - 1, i_pos - 1)));
+                                auto s = aa + ab;
+                                if (ba * (ab / s) <= max(small_num, eps * (bb * (aa / s))))
+                                {
+                                    A(i_pos, i_pos - 1) = zero;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            i_bulge_start = (i_pos_last + 4 > n) ? (i_pos_last + 4 - n) / 2 : 0;
+
+            // The following code performs the delayed update from the left
+            // it is optimized for column oriented matrices, but the increased complexity
+            // likely causes slower code
+            // for (idx_t j = i_pos_block; j < istop_m; ++j)
+            // {
+            //     idx_t i_bulge_start2 = (i_pos_last + 2 > j) ? (i_pos_last + 2 - j) / 2 : 0;
+            //     i_bulge_start2 = std::max(i_bulge_start,i_bulge_start2);
+            //     for (idx_t i_bulge = i_bulge_start2; i_bulge < n_bulges; ++i_bulge)
+            //     {
+            //         idx_t i_pos = i_pos_last - 2 * i_bulge;
+            //         auto v = col(V, i_bulge);
+            //         auto sum = A(i_pos, j) + conj(v[1]) * A(i_pos + 1, j) + conj(v[2]) * A(i_pos + 2, j);
+            //         A(i_pos, j) = A(i_pos, j) - sum * conj(v[0]);
+            //         A(i_pos + 1, j) = A(i_pos + 1, j) - sum * conj(v[0]) * v[1];
+            //         A(i_pos + 2, j) = A(i_pos + 2, j) - sum * conj(v[0]) * v[2];
+            //     }
+            // }
+
+            // Delayed update from the left
+            for (idx_t i_bulge = i_bulge_start; i_bulge < n_bulges; ++i_bulge)
+            {
+                idx_t i_pos = i_pos_last - 2 * i_bulge;
+                auto v = col(V, i_bulge);
+                for (idx_t j = i_pos + 1; j < n; ++j)
+                {
+                    auto sum = A(i_pos, j) + conj(v[1]) * A(i_pos + 1, j) + conj(v[2]) * A(i_pos + 2, j);
+                    A(i_pos, j) = A(i_pos, j) - sum * conj(v[0]);
+                    A(i_pos + 1, j) = A(i_pos + 1, j) - sum * conj(v[0]) * v[1];
+                    A(i_pos + 2, j) = A(i_pos + 2, j) - sum * conj(v[0]) * v[2];
+                }
+            }
+
+            // Accumulate the reflectors into U
+            for (idx_t i_bulge = i_bulge_start; i_bulge < n_bulges; ++i_bulge)
+            {
+                idx_t i_pos = i_pos_last - 2 * i_bulge;
+                auto v = col(V, i_bulge);
+                idx_t i1 = (i_pos - 1) - (i_pos_last - 1 - ns + 2);
+                idx_t i2 = min(nrows(Q), (i_pos_last - 1) + (i_pos_last - 1 - ns + 2) + 3);
+                for (idx_t j = i1; j < i2; ++j)
+                {
+                    auto sum = Q(j, i_pos - 1) + v[1] * Q(j, i_pos) + v[2] * Q(j, i_pos + 1);
+                    Q(j, i_pos - 1) = Q(j, i_pos - 1) - sum * v[0];
+                    Q(j, i_pos) = Q(j, i_pos) - sum * v[0] * conj(v[1]);
+                    Q(j, i_pos + 1) = Q(j, i_pos + 1) - sum * v[0] * conj(v[2]);
                 }
             }
         }
--- test/src/test_recursive_bulge_move.cpp
+++ test/src/test_recursive_bulge_move.cpp
@@ -32,22 +32,29 @@ TEMPLATE_LIST_TEST_CASE("Moves bulges is backward stable (or as stable as can be
     // Number of shifts
     idx_t ns = GENERATE(2, 4, 6);
     // Number of positions to move the shifts
-    idx_t np = GENERATE(1);
+    idx_t np = GENERATE(1, 2, 4);
     // size of matrix
-    idx_t n = ns + 2 + np;
+    idx_t n = ns + 1 + np;
     // Number of bulges
     idx_t nb = ns / 2;
+
+    const real_t eps = uroundoff<real_t>();
+    const real_t tol = 1.0e2 * n * eps;
 
     // Define the matrices and vectors
     std::unique_ptr<T[]> A_(new T[n * n]);
     std::unique_ptr<T[]> V_(new T[3 * nb]);
     std::unique_ptr<T[]> A_copy_(new T[n * n]);
-    std::unique_ptr<T[]> Q_(new T[(n - 2) * (n - 2)]);
+    std::unique_ptr<T[]> Q_(new T[n * n]);
+    std::unique_ptr<T[]> Q1_(new T[n * n]);
+    std::unique_ptr<T[]> Q2_(new T[(n - 1) * (n - 1)]);
 
     // This only works for legacy matrix, we really need to work on that construct_matrix function
     auto A = legacyMatrix<T, layout<matrix_t>>(n, n, &A_[0], n);
     auto A_copy = legacyMatrix<T, layout<matrix_t>>(n, n, &A_copy_[0], n);
-    auto Q = legacyMatrix<T, layout<matrix_t>>(n - 2, n - 2, &Q_[0], n - 2);
+    auto Q = legacyMatrix<T, layout<matrix_t>>(n, n, &Q_[0], n);
+    auto Q1 = legacyMatrix<T, layout<matrix_t>>(n, n, &Q1_[0], n);
+    auto Q2 = legacyMatrix<T, layout<matrix_t>>(n - 1, n - 1, &Q2_[0], n - 1);
     auto V = legacyMatrix<T, layout<matrix_t>>(3, nb, &V_[0], layout<matrix_t> == Layout ::ColMajor ? 3 : nb);
     std::vector<std::complex<real_t>> s(ns);
 
@@ -61,106 +68,45 @@ TEMPLATE_LIST_TEST_CASE("Moves bulges is backward stable (or as stable as can be
 
     tlapack::lacpy(Uplo::General, A, A_copy);
 
-    // Introduce some bulges into the matrix
-    for (idx_t i2 = nb; i2 > 0; --i2)
-    {
-        // Generate random householder reflector
-        T tau;
-        idx_t i = i2 - 1;
-        auto v = col(V, i);
-        v[0] = rand_helper<T>(gen);
-        v[1] = rand_helper<T>(gen);
-        v[2] = rand_helper<T>(gen);
-        larfg(v, tau);
-        v[0] = tau;
-
-        // Partially apply reflector to A
-        idx_t i_pos = 2 * i;
-        auto t0 = v[0];
-        auto t1 = t0 * conj(v[1]);
-        auto t2 = t0 * conj(v[2]);
-        for (idx_t j = 0; j < i_pos + 3; ++j)
-        {
-            auto sum = A(j, i_pos) + v[1] * A(j, i_pos + 1) + v[2] * A(j, i_pos + 2);
-            A(j, i_pos) = A(j, i_pos) - sum * t0;
-            A(j, i_pos + 1) = A(j, i_pos + 1) - sum * t1;
-            A(j, i_pos + 2) = A(j, i_pos + 2) - sum * t2;
-        }
-
-        // Fully apply reflector to A_copy
-        for (idx_t j = 0; j < n; ++j)
-        {
-            auto sum = A_copy(j, i_pos) + v[1] * A_copy(j, i_pos + 1) + v[2] * A_copy(j, i_pos + 2);
-            A_copy(j, i_pos) = A_copy(j, i_pos) - sum * t0;
-            A_copy(j, i_pos + 1) = A_copy(j, i_pos + 1) - sum * t1;
-            A_copy(j, i_pos + 2) = A_copy(j, i_pos + 2) - sum * t2;
-        }
+    // Pick some shifts to use
+    for( idx_t i = 0; i < ns; i = i+2 ){
+        s[i] = std::complex<real_t>( (real_t) i, (real_t) +1 );
+        s[i+1] = std::complex<real_t>( (real_t) i, (real_t) -1 );
     }
 
-    move_bulges(A, s, Q, V);
+    introduce_bulges( A, s, Q1, V );
 
-    std::unique_ptr<T[]> _res_orth(new T[(n - 2) * (n - 2)]);
-    auto res_orth = legacyMatrix<T, layout<matrix_t>>(n-2, n-2, &_res_orth[0], n-2);
+    // laset(Uplo::General, (T) 0.0, (T) 1.0, Q2);
+    move_bulges(A, s, Q2, V);
 
+    // Merge Q1 and Q2 into Q
+    lacpy( Uplo::General, Q1, Q );
+    auto Q1_slice = slice( Q1, pair{0,n}, pair{1,n} );
+    auto Q_slice = slice( Q, pair{0,n}, pair{1,n} );
+    gemm( Op::NoTrans, Op::NoTrans, (real_t) 1.0, Q1_slice, Q2, (real_t) 0.0, Q_slice );
+
+    remove_bulges(A, s, Q2, V);
+
+    lacpy( Uplo::General, Q, Q1 );
+    gemm( Op::NoTrans, Op::NoTrans, (real_t) 1.0, Q1_slice, Q2, (real_t) 0.0, Q_slice );
+
+    std::unique_ptr<T[]> _res_orth(new T[n * n]);
+    auto res_orth = legacyMatrix<T, layout<matrix_t>>(n, n, &_res_orth[0], n);
     auto orth_res_norm = check_orthogonality(Q, res_orth);
-        // CHECK(orth_res_norm <= tol);
+    CHECK(orth_res_norm <= tol);
 
     std::unique_ptr<T[]> _Q2(new T[n * n]);
     std::unique_ptr<T[]> _res(new T[n * n]);
     std::unique_ptr<T[]> _work(new T[n * n]);
-    auto Q2 = legacyMatrix<T, layout<matrix_t>>(n, n, &_Q2[0], n);
     auto res = legacyMatrix<T, layout<matrix_t>>(n, n, &_res[0], n);
     auto work = legacyMatrix<T, layout<matrix_t>>(n, n, &_work[0], n);
     auto normA = tlapack::lange(tlapack::frob_norm, A_copy);
 
-    // Copy Q into larger Q2 so it is easier to work with.
-    laset( Uplo::General, (T)0.0, (T)1.0, Q2 );
-    auto Q2_slice = slice( Q2, pair{1,n-1}, pair{1,n-1});
-    lacpy( Uplo::General, Q, Q2_slice);
-
-    laset( Uplo::General, (T)0.0, (T)0.0, res );
-    laset( Uplo::General, (T)0.0, (T)0.0, work );
-    
-    gemm(Op::ConjTrans, Op::NoTrans, (T)1.0, Q2, A_copy, (T)0.0, work);
-    gemm(Op::NoTrans, Op::NoTrans, (T)1.0, work, Q2, (T)0.0, res);
-
-    // Undo the partial application of the reflectors in A so we can check for errors
-    // for (idx_t i = 0; i < nb; ++i)
-    // {
-    //     auto v = col(V, i);
-    //     idx_t i_pos = 2 * i + np;
-    //     auto t0 = conj(v[0]);
-    //     auto t1 = t0 * conj(v[1]);
-    //     auto t2 = t0 * conj(v[2]);
-    //     for (idx_t j = 0; j < i_pos + 3; ++j)
-    //     {
-    //         auto sum = A(j, i_pos) + v[1] * A(j, i_pos + 1) + v[2] * A(j, i_pos + 2);
-    //         A(j, i_pos) = A(j, i_pos) - sum * t0;
-    //         A(j, i_pos + 1) = A(j, i_pos + 1) - sum * t1;
-    //         A(j, i_pos + 2) = A(j, i_pos + 2) - sum * t2;
-    //     }
-    // }
-    
-    // // Undo the full application of the reflectors in res so we can check for errors
-    // for (idx_t i = 0; i < nb; ++i)
-    // {
-    //     auto v = col(V, i);
-    //     idx_t i_pos = 2 * i + np;
-    //     auto t0 = conj(v[0]);
-    //     auto t1 = t0 * conj(v[1]);
-    //     auto t2 = t0 * conj(v[2]);
-    //     for (idx_t j = 0; j < n; ++j)
-    //     {
-    //         auto sum = res(j, i_pos) + v[1] * res(j, i_pos + 1) + v[2] * res(j, i_pos + 2);
-    //         res(j, i_pos) = res(j, i_pos) - sum * t0;
-    //         res(j, i_pos + 1) = res(j, i_pos + 1) - sum * t1;
-    //         res(j, i_pos + 2) = res(j, i_pos + 2) - sum * t2;
-    //     }
-    // }
-
-    // for(idx_t j = 0; j < n; ++j)
-    //     for(idx_t i =0;i<n;++i)
-    //         res(i,j) -= A(i,j);
-
-    int t = 2;
+    gemm(Op::ConjTrans, Op::NoTrans, (T)1.0, Q, A_copy, (T)0.0, work);
+    gemm(Op::NoTrans, Op::NoTrans, (T)1.0, work, Q, (T)0.0, res);
+    for(idx_t j = 0; j < n; ++j)
+        for(idx_t i =0;i<n;++i)
+            res(i,j) -= A(i,j);
+    auto simil_res_norm = tlapack::lange(tlapack::frob_norm, res);
+    CHECK(simil_res_norm <= tol * normA);
 }
