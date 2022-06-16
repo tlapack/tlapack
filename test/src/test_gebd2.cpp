35815658614362f103f5ae5ef1f92b291d83f267
Yuxin Cai <yuxin.cai@ucdenver.edu>
2022-06-16 11:16:38 -0600

    fix typo in tester header brief

--- test/src/test_gebd2.cpp
+++ test/src/test_gebd2.cpp
@@ -1,5 +1,5 @@
 /// @file test_gebd2.cpp
-/// @brief Test GEDB2 using UNG2R and UNGL2. Output an upper bidiagonal matrix B for a m-by-n matrix A (m >= n).
+/// @brief Test GEBD2 using UNG2R and UNGL2. Output an upper bidiagonal matrix B for a m-by-n matrix A (m >= n).
 //
 // Copyright (c) 2022, University of Colorado Denver. All rights reserved.
 //
