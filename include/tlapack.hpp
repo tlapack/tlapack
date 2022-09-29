2b2e43b3f0f47b185da1a08d6d6c6e0505275a1a
alilotfi90 <ali.lotfi@colorado.edu>
2022-09-29 15:07:28 -0600

    getri_methodD started

--- include/tlapack.hpp
+++ include/tlapack.hpp
@@ -148,8 +148,8 @@
 // LU
 // ----------------
 
-#include "tlapack/lapack/getrf.hpp"
+// #include "tlapack/lapack/getrf.hpp"
 #include "tlapack/lapack/getrf2.hpp"
-#include "tlapack/lapack/getri.hpp"
+// #include "tlapack/lapack/getri.hpp"
 #include "tlapack/lapack/ul_mult.hpp"
 #endif // TLAPACK_HH
