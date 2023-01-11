307c4130ffba55338935290d35c9df0557d06ca2
Weslley S Pereira <weslley.spereira@gmail.com>
2023-01-11 08:41:08 -0700

    Testing automatic update of pages

--- test/src/test_blocked_francis.cpp
+++ test/src/test_blocked_francis.cpp
@@ -4,8 +4,8 @@
 //
 // Copyright (c) 2021-2023, University of Colorado Denver. All rights reserved.
 //
-// This file is part of testBLAS.
-// testBLAS is free software: you can redistribute it and/or modify it under
+// This file is part of <T>LAPACK.
+// <T>LAPACK is free software: you can redistribute it and/or modify it under
 // the terms of the BSD 3-Clause license. See the accompanying LICENSE file.
 
 #include <catch2/catch_template_test_macros.hpp>
