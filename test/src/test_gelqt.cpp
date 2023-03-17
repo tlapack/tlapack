0fd350ad64007ac9f285bdc6ea6c610511b935d3
thijs <steelthijs@gmail.com>
2023-03-17 08:42:47 +0100

    split gelqf into gelqf and gelqt

--- test/src/test_gelqt.cpp
+++ test/src/test_gelqt.cpp
@@ -1,4 +1,4 @@
-/// @file test_gelqf.cpp
+/// @file test_gelqt.cpp
 /// @author Yuxin Cai, University of Colorado Denver, USA
 /// @brief Test GELQF and UNGL2 and output a k-by-n orthogonal matrix Q.
 //
@@ -20,7 +20,7 @@
 
 // Other routines
 #include <tlapack/blas/gemm.hpp>
-#include <tlapack/lapack/gelqf.hpp>
+#include <tlapack/lapack/gelqt.hpp>
 #include <tlapack/lapack/ungl2.hpp>
 
 using namespace tlapack;
@@ -43,16 +43,13 @@ TEMPLATE_TEST_CASE("LQ factorization of a general m-by-n matrix, blocked",
     const T zero(0);
 
     idx_t m, n, k, nb;
-    bool use_TT;
 
     m = GENERATE(10, 20, 30);
     n = GENERATE(10, 20, 30);
     k = GENERATE(
         8, 10, 20,
         30);  // k is the number of rows for output Q. Can personalize it.
-    nb = GENERATE(1, 2, 3, 7,
-                  12);  // nb is the block height. Can personalize it.
-    use_TT = GENERATE(true, false);
+    nb = GENERATE(2, 3, 7, 12);  // nb is the block height. Can personalize it.
 
     const real_t eps = ulp<real_t>();
     const real_t tol = real_t(m * n) * eps;
@@ -61,16 +58,17 @@ TEMPLATE_TEST_CASE("LQ factorization of a general m-by-n matrix, blocked",
     auto A = new_matrix(A_, m, n);
     std::vector<T> A_copy_;
     auto A_copy = new_matrix(A_copy_, m, n);
+    std::vector<T> TT_;
+    auto TT = new_matrix(TT_, m, nb);
     std::vector<T> Q_;
     auto Q = new_matrix(Q_, k, n);
 
     std::vector<T> tauw(min(m, n));
 
     // Workspace computation:
-    gelqf_opts_t<idx_t> workOpts;
-    workOpts.nb = nb;
+    gelqt_opts_t workOpts;
     workinfo_t workinfo;
-    gelqf_worksize(A, tauw, workinfo, workOpts);
+    gelqt_worksize(A, TT, workinfo, workOpts);
     ungl2_worksize(Q, tauw, workinfo, workOpts);
 
     // Workspace allocation:
@@ -86,9 +84,18 @@ TEMPLATE_TEST_CASE("LQ factorization of a general m-by-n matrix, blocked",
     if (k <= n)  // k must be less than or equal to n, because we cannot get a Q
                  // bigger than n-by-n
     {
-        INFO("m = " << m << " n = " << n << " k = " << k << " nb = " << nb);
+        DYNAMIC_SECTION("m = " << m << " n = " << n << " k = " << k
+                               << " nb = " << nb)
         {
-            gelqf(A, tauw, workOpts);
+            gelqt(A, TT, workOpts);
+
+            // Build tauw vector from matrix TT
+            for (idx_t j = 0; j < min(m, n); j += nb) {
+                idx_t ib = std::min<idx_t>(nb, min(m, n) - j);
+
+                for (idx_t i = 0; i < ib; i++)
+                    tauw[i + j] = TT(i + j, i);
+            }
 
             // Q is sliced down to the desired size of output Q (k-by-n).
             // It stores the desired number of Householder reflectors that UNGL2
