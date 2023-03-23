75648e6c6fd5cbce9aeee476a5c864cdb3882cb2
thijs <steelthijs@gmail.com>
2023-03-23 13:36:21 +0100

    first attempt at geqr2

---
+++ include/tlapack/lapack/gerq2.hpp
@@ -0,0 +1,137 @@
+/// @file gerq2.hpp
+/// @author Thijs Steel, KU Leuven, Belgium
+/// @note Adapted from @see
+/// https://github.com/Reference-LAPACK/lapack/blob/master/SRC/zgerq2.f
+//
+// Copyright (c) 2021-2023, University of Colorado Denver. All rights reserved.
+//
+// This file is part of <T>LAPACK.
+// <T>LAPACK is free software: you can redistribute it and/or modify it under
+// the terms of the BSD 3-Clause license. See the accompanying LICENSE file.
+
+#ifndef TLAPACK_GERQ2_HH
+#define TLAPACK_GERQ2_HH
+
+#include "tlapack/base/utils.hpp"
+#include "tlapack/lapack/larf.hpp"
+#include "tlapack/lapack/larfg.hpp"
+
+namespace tlapack {
+
+/** Worspace query of gerq2()
+ *
+ * @param[in] A m-by-n matrix.
+ *
+ * @param tau Not referenced.
+ *
+ * @param[in] opts Options.
+ *
+ * @param[in,out] workinfo
+ *      On output, the amount workspace required. It is larger than or equal
+ *      to that given on input.
+ *
+ * @ingroup workspace_query
+ */
+template <class matrix_t, class vector_t>
+inline constexpr void gerq2_worksize(const matrix_t& A,
+                                     const vector_t& tau,
+                                     workinfo_t& workinfo,
+                                     const workspace_opts_t<>& opts = {})
+{
+    using idx_t = size_type<matrix_t>;
+
+    // constants
+    const idx_t n = ncols(A);
+
+    if (n > 1) {
+        auto C = cols(A, range<idx_t>{1, n});
+        larf_worksize(Side::Right, Direction::Backward, StoreV::Rowwise,
+                      col(A, 0), tau[0], C, workinfo, opts);
+    }
+}
+
+/** Computes an RQ factorization of a matrix A.
+ *
+ * The matrix Q is represented as a product of elementary reflectors
+ * \[
+ *          Q = H_1' H_2' ... H_k',
+ * \]
+ * where k = min(m,n). Each H_i has the form
+ * \[
+ *          H_i = I - tau * v * v',
+ * \]
+ * where tau is a scalar, and v is a vector with
+ * \[
+ *          v[n-k+i+1:n] = 0; v[n-k+i-1] = 1,
+ * \]
+ * with v[1] through v[n-k+i-1] stored on exit below the diagonal
+ * in the ith column of A, and tau in tau[i].
+ *
+ * @return  0 if success
+ *
+ * @param[in,out] A m-by-n matrix.
+ *      On exit, the elements on and above the diagonal of the array
+ *      contain the m-by-min(m,n) upper trapezoidal matrix R
+ *      (R is upper triangular if m <= n); the elements below the diagonal,
+ *      with the array tau, represent the unitary matrix Q as a
+ *      product of elementary reflectors.
+ * @param[out] tau Real vector of length min(m,n).
+ *      The scalar factors of the elementary reflectors.
+ *
+ * @param[in] opts Options.
+ *      - @c opts.work is used if whenever it has sufficient size.
+ *        The sufficient size can be obtained through a workspace query.
+ *
+ * @ingroup computational
+ */
+template <class matrix_t, class vector_t>
+int gerq2(matrix_t& A, vector_t& tau, const workspace_opts_t<>& opts = {})
+{
+    using idx_t = size_type<matrix_t>;
+    using pair = pair<idx_t, idx_t>;
+
+    // constants
+    const idx_t m = nrows(A);
+    const idx_t n = ncols(A);
+    const idx_t k = std::min<idx_t>(m, n);
+
+    // check arguments
+    tlapack_check_false((idx_t)size(tau) < std::min<idx_t>(m, n));
+
+    // quick return
+    if (n <= 0) return 0;
+
+    // Allocates workspace
+    vectorOfBytes localworkdata;
+    Workspace work = [&]() {
+        workinfo_t workinfo;
+        gerq2_worksize(A, tau, workinfo, opts);
+        return alloc_workspace(localworkdata, workinfo, opts.work);
+    }();
+
+    // Options to forward
+    auto&& larfOpts = workspace_opts_t<>{work};
+
+    for (idx_t i2 = 0; i2 < k; ++i2) {
+        idx_t i = k - 1 - i2;
+
+        // Define v := A[m-1-i2,0:n-i2]
+        auto v = slice(A, m - 1 - i2, pair{0, n - i2});
+
+        // Generate the (i+1)-th elementary Householder reflection on v
+        larfg(Direction::Backward, StoreV::Rowwise, v, tau[i]);
+
+        // Apply the reflector to the rest of the matrix
+        if (m > i2 + 1) {
+            auto C = slice(A, pair{0, m - 1 - i2}, pair{0, n - i2});
+            larf(Side::Right, Direction::Backward, StoreV::Rowwise, v, tau[i],
+                 C, larfOpts);
+        }
+    }
+
+    return 0;
+}
+
+}  // namespace tlapack
+
+#endif  // TLAPACK_GERQ2_HH
---
+++ include/tlapack/lapack/ungr2.hpp
@@ -0,0 +1,139 @@
+/// @file ungr2.hpp
+/// @author Thijs Steel, KU Leuven, Belgium
+/// @note Adapted from @see
+/// https://github.com/Reference-LAPACK/lapack/blob/master/SRC/zungr2.f
+//
+// Copyright (c) 2021-2023, University of Colorado Denver. All rights reserved.
+//
+// This file is part of <T>LAPACK.
+// <T>LAPACK is free software: you can redistribute it and/or modify it under
+// the terms of the BSD 3-Clause license. See the accompanying LICENSE file.
+
+#ifndef TLAPACK_UNGR2_HH
+#define TLAPACK_UNGR2_HH
+
+#include "tlapack/base/utils.hpp"
+#include "tlapack/lapack/larf.hpp"
+
+namespace tlapack {
+
+/** Worspace query of ung2r()
+ *
+ *
+ * @param[in] A m-by-n matrix.
+
+ * @param[in] tau Real vector of length min(m,n).
+ *      The scalar factors of the elementary reflectors.
+ *
+ * @param[in] opts Options.
+ *
+ * @param[in,out] workinfo
+ *      On output, the amount workspace required. It is larger than or equal
+ *      to that given on input.
+ *
+ * @ingroup workspace_query
+ */
+template <class matrix_t, class vector_t>
+inline constexpr void ungr2_worksize(const matrix_t& A,
+                                     const vector_t& tau,
+                                     workinfo_t& workinfo,
+                                     const workspace_opts_t<>& opts = {})
+{
+    using idx_t = size_type<matrix_t>;
+
+    // constants
+    const idx_t n = ncols(A);
+
+    if (n > 1) {
+        auto C = cols(A, range<idx_t>{1, n});
+        larf_worksize(left_side, forward, columnwise_storage, col(A, 0), tau[0],
+                      C, workinfo, opts);
+    }
+}
+
+/**
+ * @brief Generates a matrix Q with orthogonal columns.
+ * \[
+ *     Q  =  H_1 H_2 ... H_k
+ * \]
+ *
+ * @param[in,out] A m-by-n matrix.
+ *      On entry, the i-th column must contains the vector which defines the
+ *      elementary reflector $H_i$, for $i=0,1,...,k-1$, as returned by geqrf.
+ *      On exit, the m-by-n matrix $Q$.
+
+ * @param[in] tau Real vector of length min(m,n).
+ *      The scalar factors of the elementary reflectors.
+ *
+ * @param[in] opts Options.
+ *      @c opts.work is used if whenever it has sufficient size.
+ *      The sufficient size can be obtained through a workspace query.
+ *
+ * @return 0 if success
+ *
+ * @ingroup computational
+ */
+template <class matrix_t, class vector_t>
+int ungr2(matrix_t& A, const vector_t& tau, const workspace_opts_t<>& opts = {})
+{
+    using T = type_t<matrix_t>;
+    using real_t = real_type<T>;
+    using idx_t = size_type<matrix_t>;
+    using pair = pair<idx_t, idx_t>;
+
+    // constants
+    const real_t zero(0);
+    const real_t one(1);
+    const idx_t m = nrows(A);
+    const idx_t n = ncols(A);
+    const idx_t k = size(tau);
+
+    // check arguments
+    tlapack_check_false(k < 0 || k > n);
+
+    // quick return
+    if (n <= 0) return 0;
+
+    // Allocates workspace
+    vectorOfBytes localworkdata;
+    Workspace work = [&]() {
+        workinfo_t workinfo;
+        ungr2_worksize(A, tau, workinfo, opts);
+        return alloc_workspace(localworkdata, workinfo, opts.work);
+    }();
+
+    // Options to forward
+    auto&& larfOpts = workspace_opts_t<>{work};
+
+    // Initialise rows 0:m-k to rows of the unit matrix
+    for (idx_t j = 0; j < n; ++j) {
+        for (idx_t i = 0; i < m - k; ++i)
+            A(i, j) = zero;
+        // if (j + m >= n and j + k <= n) A(m + j - n, j) = one;
+    }
+
+    for (idx_t i = 0; i < k; ++i) {
+        idx_t ii = m - k + i;
+        if (ii > 0) {
+            auto v = slice(A, ii, pair{0, n - k + 1 + i});
+            auto C = slice(A, pair{0, ii}, pair{0, n - k + 1 + i});
+            larf(Side::Right, Direction::Backward, StoreV::Rowwise, v,
+                 conj(tau[i]), C, larfOpts);
+        }
+        if (n + i > k) {
+            auto x = slice(A, ii, pair{0, n - k + i});
+            scal(-conj(tau[i]), x);
+        }
+        A(ii, n - k + i) = one - conj(tau[i]);
+
+        // Set A( 0:i-1, i ) to zero
+        for (idx_t l = n - k + i + 1; l < n; l++)
+            A(ii, l) = zero;
+    }
+
+    return 0;
+}
+
+}  // namespace tlapack
+
+#endif  // TLAPACK_UNGR2_HH
