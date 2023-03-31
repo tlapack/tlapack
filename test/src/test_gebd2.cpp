a5523412e39c70b6a774e760c6174ccc155eaa9d
thijs <steelthijs@gmail.com>
2023-03-31 11:13:12 +0200

    add and test ungbr

--- test/src/test_gebd2.cpp
+++ test/src/test_gebd2.cpp
@@ -20,12 +20,13 @@
 #include <tlapack/lapack/lacpy.hpp>
 #include <tlapack/lapack/lange.hpp>
 #include <tlapack/lapack/laset.hpp>
-// #include <tlapack/plugins/debugutils.hpp>
+#include <tlapack/plugins/debugutils.hpp>
 
 // Other routines
 #include <tlapack/blas/gemm.hpp>
 #include <tlapack/lapack/gebd2.hpp>
 #include <tlapack/lapack/ung2r.hpp>
+#include <tlapack/lapack/ungbr.hpp>
 #include <tlapack/lapack/ungl2.hpp>
 
 using namespace tlapack;
@@ -62,9 +63,9 @@ TEMPLATE_TEST_CASE("bidiagonal reduction is backward stable",
     std::vector<T> A_copy_;
     auto A_copy = new_matrix(A_copy_, m, n);
     std::vector<T> Q_;
-    auto Q = new_matrix(Q_, m, m);
+    auto Q = new_matrix(Q_, m, k);
     std::vector<T> Z_;
-    auto Z = new_matrix(Z_, n, n);
+    auto Z = new_matrix(Z_, k, n);
 
     std::vector<T> tauv(k);
     std::vector<T> tauw(k);
@@ -83,13 +84,13 @@ TEMPLATE_TEST_CASE("bidiagonal reduction is backward stable",
 
         // Get bidiagonal B
         std::vector<T> B_;
-        auto B = new_matrix(B_, m, n);
+        auto B = new_matrix(B_, k, k);
         laset(Uplo::General, zero, zero, B);
 
         if (m >= n) {
             // copy upper bidiagonal matrix
             B(0, 0) = A(0, 0);
-            for (idx_t j = 1; j < n; ++j) {
+            for (idx_t j = 1; j < k; ++j) {
                 B(j - 1, j) = A(j - 1, j);
                 B(j, j) = A(j, j);
             }
@@ -97,55 +98,36 @@ TEMPLATE_TEST_CASE("bidiagonal reduction is backward stable",
         else {
             // copy lower bidiagonal matrix
             B(0, 0) = A(0, 0);
-            for (idx_t j = 1; j < m; ++j) {
+            for (idx_t j = 1; j < k; ++j) {
                 B(j, j - 1) = A(j, j - 1);
                 B(j, j) = A(j, j);
             }
         }
 
-        // Generate m-by-m unitary matrix Q
-        if (m >= n) {
-            lacpy(Uplo::Lower, A, Q);
-            ung2r(Q, tauv);
-        }
-        else {
-            auto Q11 = slice(Q, range(1, m), range(1, m));
-            auto X = slice(A, range(1, m), range(0, m - 1));
-            auto tauv1 = slice(tauv, range(0, k - 1));
-            laset(Uplo::General, zero, one, Q);
-            lacpy(Uplo::Lower, X, Q11);
-            ung2r(Q11, tauv1);
-        }
+        // Generate m-by-k unitary matrix Q
+        ungbr_opts_t ungbrOpts;
+        ungbrOpts.nb = 2;
+        lacpy(Uplo::Lower, slice(A, pair{0, m}, pair{0, k}), Q);
+        ungbr(QorP::Q, n, Q, tauv, ungbrOpts);
 
         // Test for Q's orthogonality
         std::vector<T> Wq_;
-        auto Wq = new_matrix(Wq_, m, m);
+        auto Wq = new_matrix(Wq_, k, k);
         auto orth_Q = check_orthogonality(Q, Wq);
-        CHECK(orth_Q / tol <= one);
+        CHECK(orth_Q <= tol);
 
-        // Generate m-by-m unitary matrix Z
-        if (m >= n) {
-            auto Z11 = slice(Z, range(1, n), range(1, n));
-            auto X = slice(A, range(0, n - 1), range(1, n));
-            auto tauw1 = slice(tauw, range(0, k - 1));
-            laset(Uplo::General, zero, one, Z);
-            lacpy(Uplo::Upper, X, Z11);
-            ungl2(Z11, tauw1);
-        }
-        else {
-            lacpy(Uplo::Upper, A, Z);
-            ungl2(Z, tauw);
-        }
+        lacpy(Uplo::Upper, slice(A, pair{0, k}, pair{0, n}), Z);
+        ungbr(QorP::P, m, Z, tauw, ungbrOpts);
 
         // Test for Z's orthogonality
         std::vector<T> Wz_;
-        auto Wz = new_matrix(Wz_, n, n);
+        auto Wz = new_matrix(Wz_, k, k);
         auto orth_Z = check_orthogonality(Z, Wz);
-        CHECK(orth_Z / tol <= one);
+        CHECK(orth_Z <= tol);
 
         // Test Q * B * Z^H = A
         std::vector<T> K_;
-        auto K = new_matrix(K_, m, n);
+        auto K = new_matrix(K_, m, k);
         laset(Uplo::General, zero, zero, K);
         gemm(Op::NoTrans, Op::NoTrans, real_t(1.), Q, B, real_t(0), K);
         gemm(Op::NoTrans, Op::NoTrans, real_t(1.), K, Z, real_t(-1.), A_copy);
