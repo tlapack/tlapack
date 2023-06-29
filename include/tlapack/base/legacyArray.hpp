7a5aaa63e04604250596c25d7598fa9a15c1e2b7
Weslley S Pereira <weslley.spereira@gmail.com>
2023-06-29 08:29:06 -0600

    Fix legacyBanded duplicate implementation

--- include/tlapack/base/legacyArray.hpp
+++ include/tlapack/base/legacyArray.hpp
@@ -14,6 +14,7 @@
 #include <cassert>
 
 #include "tlapack/base/exceptionHandling.hpp"
+#include "tlapack/base/legacyBandedMatrix.hpp"
 #include "tlapack/base/types.hpp"
 
 namespace tlapack {
@@ -111,61 +112,6 @@ struct legacyVector {
         : n(n), ptr(ptr), inc(inc)
     {
         tlapack_check_false(n < 0);
-    }
-};
-
-/** Legacy banded matrix.
- *
- * Mind that the access A(i,j) is valid if, and only if,
- *  max(0,j-ku) <= i <= min(m,j+kl)
- * This class does not perform such a check,
- * otherwise it would lack in performance.
- *
- * @tparam T Floating-point type
- * @tparam idx_t Index type
- */
-template <typename T, class idx_t = std::size_t>
-struct legacyBandedMatrix {
-    idx_t m, n, kl, ku;  ///< Sizes
-    T* ptr;              ///< Pointer to array in memory
-
-    /** Access A(i,j) = ptr[ (ku+(i-j)) + j*(ku+kl+1) ]
-     *
-     * Mind that this access is valid if, and only if,
-     *  max(0,j-ku) <= i <= min(m,j+kl)
-     * This operator does not perform such a check,
-     * otherwise it would lack in performance.
-     *
-     */
-    inline constexpr const T& operator()(idx_t i, idx_t j) const noexcept
-    {
-        assert(i >= 0);
-        assert(i < m);
-        assert(j >= 0);
-        assert(j < n);
-        assert(j <= i + ku);
-        assert(i <= j + kl);
-        return ptr[(ku + i) + j * (ku + kl)];
-    }
-
-    inline constexpr T& operator()(idx_t i, idx_t j) noexcept
-    {
-        assert(i >= 0);
-        assert(i < m);
-        assert(j >= 0);
-        assert(j < n);
-        assert(j <= i + ku);
-        assert(i <= j + kl);
-        return ptr[(ku + i) + j * (ku + kl)];
-    }
-
-    inline constexpr legacyBandedMatrix(
-        idx_t m, idx_t n, idx_t kl, idx_t ku, T* ptr)
-        : m(m), n(n), kl(kl), ku(ku), ptr(ptr)
-    {
-        tlapack_check_false(m < 0);
-        tlapack_check_false(n < 0);
-        tlapack_check_false((kl + 1 > m && m > 0) || (ku + 1 > n && n > 0));
     }
 };
 
