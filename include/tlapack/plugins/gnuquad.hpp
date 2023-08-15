af427e01196779028742d9344fd7a6845e3e6242
Weslley S Pereira <weslley.spereira@gmail.com>
2023-08-15 17:18:56 -0600

    Complete compatibility with GNU __float128

--- include/tlapack/plugins/gnuquad.hpp
+++ include/tlapack/plugins/gnuquad.hpp
@@ -1,21 +1,24 @@
-// using std::abs;
-// using std::ceil;
-// using std::floor;
-// using std::isinf;
-// using std::isnan;
-// using std::log2;
-// using std::max;
-// using std::min;
-// using std::pow;  // We only use pow(int, T), see below in the concept Real.
-// using std::sqrt;
+/// @file gnuquad.hpp Compatibility layer for the GNU type __float128
+/// @author Weslley S Pereira, University of Colorado Denver, USA
+//
+// Copyright (c) 2021-2023, University of Colorado Denver. All rights reserved.
+//
+// This file is part of <T>LAPACK.
+// <T>LAPACK is free software: you can redistribute it and/or modify it under
+// the terms of the BSD 3-Clause license. See the accompanying LICENSE file.
+
+#ifndef TLAPACK_GNUQUAD_HH
+#define TLAPACK_GNUQUAD_HH
 
 #include <quadmath.h>
 
+#include <complex>
 #include <limits>
+#include <ostream>
 
 namespace std {
 
-inline constexpr __float128 abs(__float128 x);  // See include/bits/std_abs.h
+constexpr __float128 abs(__float128 x);  // See include/bits/std_abs.h
 inline __float128 ceil(__float128 x) noexcept { return ceilq(x); }
 inline __float128 floor(__float128 x) noexcept { return floorq(x); }
 inline bool isinf(__float128 x) noexcept { return isinfq(x); }
@@ -31,6 +34,38 @@ inline __float128 min(__float128 x, __float128 y) noexcept
 }
 inline __float128 pow(int x, __float128 y) noexcept { return powq(x, y); }
 inline __float128 sqrt(__float128 x) noexcept { return sqrtq(x); }
+
+inline __float128 abs(complex<__float128> z)
+{
+    __float128 x = z.real();
+    __float128 y = z.imag();
+    const __float128 __s = max(abs(x), abs(y));
+    if (__s == __float128())  // well ...
+        return __s;
+    x /= __s;
+    y /= __s;
+    return __s * sqrt(x * x + y * y);
+}
+inline complex<__float128> sqrt(complex<__float128> z)
+{
+    const __float128 x = real(z);
+    const __float128 y = imag(z);
+    const __float128 zero(0);
+    const __float128 two(2);
+    const __float128 half(0.5);
+
+    if (x == zero) {
+        __float128 t = sqrt(half * abs(y));
+        return complex<__float128>(t, (y < zero) ? -t : t);
+    }
+    else {
+        __float128 t = sqrt(two * (abs(z) + abs(x)));
+        __float128 u = half * t;
+        return (x > zero)
+                   ? complex<__float128>(u, y / t)
+                   : complex<__float128>(abs(y) / t, (y < zero) ? -u : u);
+    }
+}
 
 template <>
 struct numeric_limits<__float128> {
@@ -83,4 +118,14 @@ struct numeric_limits<__float128> {
     static constexpr float_round_style round_style = round_to_nearest;
 };
 
+ostream& operator<<(ostream& out, const __float128& x)
+{
+    char buf[128];
+    quadmath_snprintf(buf, 128, "%+-#*.20Qe", x);
+    out << buf;
+    return out;
+}
+
 }  // namespace std
+
+#endif
