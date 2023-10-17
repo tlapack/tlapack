3c4ff59af1acd992aba2092d30e3118945bb6afe
thijssteel <thijs.steel@kuleuven.be>
2023-10-17 13:32:39 +0200

    full qz sweep

--- include/tlapack/lapack/multishift_qz_sweep.hpp
+++ include/tlapack/lapack/multishift_qz_sweep.hpp
@@ -368,10 +368,10 @@ void multishift_QZ_sweep(bool want_t,
     // The following code block moves the bulges down until they are low enough
     // to be removed
     //
-    while (i_pos_block + n_block_desired + 1 < ihi) {
+    while (i_pos_block + n_block_desired < ihi) {
         // Number of positions each bulge will be moved down
         idx_t n_pos = std::min<idx_t>(n_block_desired - n_shifts,
-                                      ihi - n_shifts - 1 - i_pos_block);
+                                      ihi - n_shifts - i_pos_block);
         // Actual blocksize
         idx_t n_block = n_shifts + n_pos;
 
@@ -575,6 +575,276 @@ void multishift_QZ_sweep(bool want_t,
         }
 
         i_pos_block = i_pos_block + n_pos;
+    }
+
+    //
+    // The following code removes the bulges from the matrix
+    //
+    {
+        // Actual blocksize
+        idx_t n_block = ihi - i_pos_block;
+
+        auto Qc2 = slice(Qc, range{0, n_block}, range{0, n_block});
+        laset(GENERAL, zero, one, Qc2);
+        auto Zc2 = slice(Zc, range{0, n_block}, range{0, n_block});
+        laset(GENERAL, zero, one, Zc2);
+
+        // The calculations are initially limited to a small window
+        // The rest is updated later via level 3 BLAS
+        auto A2 = slice(A, range(i_pos_block, ihi), range(i_pos_block, ihi));
+        auto B2 = slice(B, range(i_pos_block, ihi), range(i_pos_block, ihi));
+
+        for (idx_t i_bulge = 0; i_bulge < n_bulges; i_bulge++) {
+            TA t1;
+            idx_t i2 = 2 * (n_bulges - i_bulge - 1);
+            for (idx_t i = i2; i < n_block - 1; ++i) {
+                // Remove fill-in from B using an inverse reflector
+                if (i + 2 < n_block) {
+                    auto T = slice(B2, range{i, i + 3}, range{i, i + 3});
+                    inv_house3(T, v, t1);
+
+                    // Apply the reflector
+                    {
+                        t1 = conj(t1);
+                        const TA v2 = v[1];
+                        const TA t2 = t1 * v2;
+                        const TA v3 = v[2];
+                        const TA t3 = t1 * v[2];
+                        TA sum;
+
+                        // Apply reflector from the right to B
+                        for (idx_t j = 0; j < i + 3; ++j) {
+                            sum = B2(j, i) + v2 * B2(j, i + 1) +
+                                  v3 * B2(j, i + 2);
+                            B2(j, i) = B2(j, i) - sum * conj(t1);
+                            B2(j, i + 1) = B2(j, i + 1) - sum * conj(t2);
+                            B2(j, i + 2) = B2(j, i + 2) - sum * conj(t3);
+                        }
+                        B2(i + 1, i) = (TA)0;
+                        B2(i + 2, i) = (TA)0;
+                        // Apply reflector from the right to A
+                        for (idx_t j = 0; j < min(n_block, i + 4); ++j) {
+                            sum = A2(j, i) + v2 * A2(j, i + 1) +
+                                  v3 * A2(j, i + 2);
+                            A2(j, i) = A2(j, i) - sum * conj(t1);
+                            A2(j, i + 1) = A2(j, i + 1) - sum * conj(t2);
+                            A2(j, i + 2) = A2(j, i + 2) - sum * conj(t3);
+                        }
+                        // Apply reflector to Zc from the right
+                        for (idx_t j = 0; j < n_block; ++j) {
+                            sum = Zc2(j, i) + v2 * Zc2(j, i + 1) +
+                                  v3 * Zc2(j, i + 2);
+                            Zc2(j, i) = Zc2(j, i) - sum * conj(t1);
+                            Zc2(j, i + 1) = Zc2(j, i + 1) - sum * conj(t2);
+                            Zc2(j, i + 2) = Zc2(j, i + 2) - sum * conj(t3);
+                        }
+                    }
+                }
+                else {
+                    // For the final 2x2 block, use a rotation instead of a
+                    // reflector
+                    real_t c2;
+                    TA s2;
+                    rotg(B2(i + 1, i + 1), B2(i + 1, i), c2, s2);
+                    s2 = -s2;
+                    B2(i + 1, i) = (TA)0.;
+
+                    // Apply rotation
+                    {
+                        auto b1 = slice(B2, range{0, i + 1}, i);
+                        auto b2 = slice(B2, range{0, i + 1}, i + 1);
+                        rot(b1, b2, c2, conj(s2));
+                        auto a1 = col(A2, i);
+                        auto a2 = col(A2, i + 1);
+                        rot(a1, a2, c2, conj(s2));
+                        if (want_z) {
+                            auto z1 = col(Zc2, i);
+                            auto z2 = col(Zc2, i + 1);
+                            rot(z1, z2, c2, conj(s2));
+                        }
+                    }
+                }
+
+                // Calculate a reflector to move the bulge one position
+                if (i + 2 < n_block) {
+                    if (i + 3 < n_block) {
+                        // Normal size bulge
+                        v[0] = A2(i + 1, i);
+                        v[1] = A2(i + 2, i);
+                        v[2] = A2(i + 3, i);
+                        larfg(FORWARD, COLUMNWISE_STORAGE, v, t1);
+                        A2(i + 1, i) = v[0];
+                        A2(i + 2, i) = zero;
+                        A2(i + 3, i) = zero;
+
+                        // Apply reflector
+                        {
+                            t1 = conj(t1);
+                            const TA v2 = v[1];
+                            const TA t2 = t1 * v2;
+                            const TA v3 = v[2];
+                            const TA t3 = t1 * v[2];
+                            TA sum;
+
+                            // Apply reflector from the left to A
+                            for (idx_t j = i + 1; j < n_block; ++j) {
+                                sum = A2(i + 1, j) + conj(v2) * A2(i + 2, j) +
+                                      conj(v3) * A2(i + 3, j);
+                                A2(i + 1, j) = A2(i + 1, j) - sum * t1;
+                                A2(i + 2, j) = A2(i + 2, j) - sum * t2;
+                                A2(i + 3, j) = A2(i + 3, j) - sum * t3;
+                            }
+                            // Apply reflector from the left to B
+                            for (idx_t j = i + 1; j < n_block; ++j) {
+                                sum = B2(i + 1, j) + conj(v2) * B2(i + 2, j) +
+                                      conj(v3) * B2(i + 3, j);
+                                B2(i + 1, j) = B2(i + 1, j) - sum * t1;
+                                B2(i + 2, j) = B2(i + 2, j) - sum * t2;
+                                B2(i + 3, j) = B2(i + 3, j) - sum * t3;
+                            }
+                            // Apply reflector to Qc from the right
+                            for (idx_t j = 0; j < n_block; ++j) {
+                                sum = Qc2(j, i + 1) + v2 * Qc2(j, i + 2) +
+                                      v3 * Qc2(j, i + 3);
+                                Qc2(j, i + 1) = Qc2(j, i + 1) - sum * conj(t1);
+                                Qc2(j, i + 2) = Qc2(j, i + 2) - sum * conj(t2);
+                                Qc2(j, i + 3) = Qc2(j, i + 3) - sum * conj(t3);
+                            }
+                        }
+                    }
+                    else {
+                        // Small 2x2 bulge
+                        v[0] = A2(i + 1, i);
+                        v[1] = A2(i + 2, i);
+                        v[2] = zero;
+                        larfg(FORWARD, COLUMNWISE_STORAGE, v, t1);
+                        A2(i + 1, i) = v[0];
+                        A2(i + 2, i) = zero;
+
+                        // Apply reflector
+                        {
+                            t1 = conj(t1);
+                            const TA v2 = v[1];
+                            const TA t2 = t1 * v2;
+                            TA sum;
+
+                            // Apply reflector from the left to A
+                            for (idx_t j = i + 1; j < n_block; ++j) {
+                                sum = A2(i + 1, j) + conj(v2) * A2(i + 2, j);
+                                A2(i + 1, j) = A2(i + 1, j) - sum * t1;
+                                A2(i + 2, j) = A2(i + 2, j) - sum * t2;
+                            }
+                            // Apply reflector from the left to B
+                            for (idx_t j = i + 1; j < n_block; ++j) {
+                                sum = B2(i + 1, j) + conj(v2) * B2(i + 2, j);
+                                B2(i + 1, j) = B2(i + 1, j) - sum * t1;
+                                B2(i + 2, j) = B2(i + 2, j) - sum * t2;
+                            }
+                            // Apply reflector to Qc from the right
+                            for (idx_t j = 0; j < n_block; ++j) {
+                                sum = Qc2(j, i + 1) + v2 * Qc2(j, i + 2);
+                                Qc2(j, i + 1) = Qc2(j, i + 1) - sum * conj(t1);
+                                Qc2(j, i + 2) = Qc2(j, i + 2) - sum * conj(t2);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        // Update rest of the matrix
+        idx_t istart_m, istop_m;
+        if (want_t) {
+            istart_m = 0;
+            istop_m = n;
+        }
+        else {
+            istart_m = ilo;
+            istop_m = ihi;
+        }
+        // Update A
+        if (i_pos_block + n_block < istop_m) {
+            idx_t i = i_pos_block + n_block;
+            while (i < istop_m) {
+                idx_t iblock = std::min<idx_t>(istop_m - i, ncols(WH));
+                auto A_slice =
+                    slice(A, range{i_pos_block + 1, i_pos_block + 1 + n_block},
+                          range{i, i + iblock});
+                auto WH_slice = slice(WH, range{0, nrows(A_slice)},
+                                      range{0, ncols(A_slice)});
+                gemm(CONJ_TRANS, NO_TRANS, one, Qc2, A_slice, WH_slice);
+                lacpy(GENERAL, WH_slice, A_slice);
+                i = i + iblock;
+            }
+        }
+        if (istart_m < i_pos_block) {
+            idx_t i = istart_m;
+            while (i < i_pos_block) {
+                idx_t iblock = std::min<idx_t>(i_pos_block - i, nrows(WV));
+                auto A_slice = slice(A, range{i, i + iblock},
+                                     range{i_pos_block, i_pos_block + n_block});
+                auto WV_slice = slice(WV, range{0, nrows(A_slice)},
+                                      range{0, ncols(A_slice)});
+                gemm(NO_TRANS, NO_TRANS, one, A_slice, Zc2, WV_slice);
+                lacpy(GENERAL, WV_slice, A_slice);
+                i = i + iblock;
+            }
+        }
+        // Update B
+        if (i_pos_block + n_block < istop_m) {
+            idx_t i = i_pos_block + n_block;
+            while (i < istop_m) {
+                idx_t iblock = std::min<idx_t>(istop_m - i, ncols(WH));
+                auto B_slice =
+                    slice(B, range{i_pos_block + 1, i_pos_block + 1 + n_block},
+                          range{i, i + iblock});
+                auto WH_slice = slice(WH, range{0, nrows(B_slice)},
+                                      range{0, ncols(B_slice)});
+                gemm(CONJ_TRANS, NO_TRANS, one, Qc2, B_slice, WH_slice);
+                lacpy(GENERAL, WH_slice, B_slice);
+                i = i + iblock;
+            }
+        }
+        if (istart_m < i_pos_block) {
+            idx_t i = istart_m;
+            while (i < i_pos_block) {
+                idx_t iblock = std::min<idx_t>(i_pos_block - i, nrows(WV));
+                auto B_slice = slice(B, range{i, i + iblock},
+                                     range{i_pos_block, i_pos_block + n_block});
+                auto WV_slice = slice(WV, range{0, nrows(B_slice)},
+                                      range{0, ncols(B_slice)});
+                gemm(NO_TRANS, NO_TRANS, one, B_slice, Zc2, WV_slice);
+                lacpy(GENERAL, WV_slice, B_slice);
+                i = i + iblock;
+            }
+        }
+        // Update Q
+        if (want_q) {
+            idx_t i = 0;
+            while (i < n) {
+                idx_t iblock = std::min<idx_t>(n - i, nrows(WV));
+                auto Q_slice = slice(Q, range{i, i + iblock},
+                                     range{i_pos_block, i_pos_block + n_block});
+                auto WV_slice = slice(WV, range{0, nrows(Q_slice)},
+                                      range{0, ncols(Q_slice)});
+                gemm(NO_TRANS, NO_TRANS, one, Q_slice, Qc2, WV_slice);
+                lacpy(GENERAL, WV_slice, Q_slice);
+                i = i + iblock;
+            }
+        }
+        // Update Z
+        if (want_z) {
+            idx_t i = 0;
+            while (i < n) {
+                idx_t iblock = std::min<idx_t>(n - i, nrows(WV));
+                auto Z_slice = slice(Z, range{i, i + iblock},
+                                     range{i_pos_block, i_pos_block + n_block});
+                auto WV_slice = slice(WV, range{0, nrows(Z_slice)},
+                                      range{0, ncols(Z_slice)});
+                gemm(NO_TRANS, NO_TRANS, one, Z_slice, Zc2, WV_slice);
+                lacpy(GENERAL, WV_slice, Z_slice);
+                i = i + iblock;
+            }
+        }
     }
 }
 
