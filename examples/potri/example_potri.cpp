fffffa4f0baeead504b837c33f523ad5f0729a00
Addison-Taylor <addisoel@MATH-G9Z3DS2.ucdenver.pvt>
2025-06-05 08:58:56 -0600

    working towards potri

---
+++ examples/potri/example_potri.cpp
@@ -0,0 +1,372 @@
+/// @file example_geqr2.cpp
+/// @author Weslley S Pereira, University of Colorado Denver, USA
+//
+// Copyright (c) 2025, University of Colorado Denver. All rights reserved.
+//
+// This file is part of <T>LAPACK.
+// <T>LAPACK is free software: you can redistribute it and/or modify it under
+// the terms of the BSD 3-Clause license. See the accompanying LICENSE file.
+
+// Plugins for <T>LAPACK (must come before <T>LAPACK headers)
+#include <tlapack/plugins/legacyArray.hpp>
+
+// <T>LAPACK
+#include <tlapack/blas/syrk.hpp>
+#include <tlapack/blas/trmm.hpp>
+#include <tlapack/lapack/geqr2.hpp>
+#include <tlapack/lapack/lacpy.hpp>
+#include <tlapack/lapack/lange.hpp>
+#include <tlapack/lapack/lansy.hpp>
+#include <tlapack/lapack/laset.hpp>
+#include <tlapack/lapack/ung2r.hpp>
+#include <tlapack/lapack/potrf.hpp>
+#include <tlapack/blas/trsm.hpp>
+#include <tlapack/blas/hemm.hpp>
+#include <tlapack/lapack/lanhe.hpp>
+#include <tlapack/lapack/trtri_recursive.hpp>
+#include <tlapack/lapack/lauum_recursive.hpp>
+
+// C++ headers
+#include <chrono>  // for high_resolution_clock
+#include <iostream>
+#include <memory>
+#include <vector>
+
+using namespace tlapack;
+
+//------------------------------------------------------------------------------
+/// Print matrix A in the standard output
+template <typename matrix_t>
+void printMatrix(const matrix_t& A)
+{
+    using idx_t = size_type<matrix_t>;
+    const idx_t m = nrows(A);
+    const idx_t n = ncols(A);
+
+    for (idx_t i = 0; i < m; ++i) {
+        std::cout << std::endl;
+        for (idx_t j = 0; j < n; ++j)
+            std::cout << A(i, j) << " ";
+    }
+}
+//------------------------------------------------------------------------------
+
+template <TLAPACK_SMATRIX matrix_t>
+void mult_chc_level0(matrix_t& C) 
+{
+
+    using idx_t = size_type<matrix_t>;
+    using T = type_t<matrix_t>;
+    using real_t = real_type<T>;
+
+    const idx_t m = nrows(C);
+    const idx_t n = ncols(C);
+
+    if (m != n) 
+        return;
+    else
+    {
+        for (idx_t j = n; j-- > 0; ) {
+            real_t real_part_of_cjj;
+            real_part_of_cjj = real(C(j, j)) * real(C(j, j));
+            for(idx_t k = 0; k < j; ++k) {
+                real_part_of_cjj += real(C(k, j)) * real(C(k, j)) + imag(C(k, j)) * imag(C(k, j));
+            }
+            C(j,j) = real_part_of_cjj;
+            for (idx_t i = j; i-- > 0; ) {
+                C(i, j) = conj(C(i, i)) * C(i, j);
+                for (idx_t k = i; k-- > 0; ) {
+                    C(i, j) += conj(C(k, i)) * C(k, j);
+                }
+            }
+        }
+    
+    return;
+    }
+}
+
+//----------------------------------------------------------
+template <TLAPACK_SMATRIX matrix_t>
+void mult_chc(matrix_t& A)
+{
+    using T = type_t<matrix_t>;
+    typedef tlapack::real_type<T> real_t;
+    using idx_t = tlapack::size_type<matrix_t>;
+    using range = pair<idx_t, idx_t>;
+
+    const idx_t m = nrows(A);
+    const idx_t n = ncols(A);
+
+    if (m != n)
+        return;
+
+    if (n <= 1){
+        A(0,0) = real(A(0,0))*real(A(0,0));
+        return;
+    }
+
+    const idx_t n0 = n/2;
+
+    auto A00 = slice(A, range(0, n0), range(0, n0));
+    auto A01 = slice(A, range(0, n0), range(n0, n));
+    auto A11 = slice(A, range(n0, n), range(n0, n));
+
+    mult_chc(A11);
+
+    herk(Uplo::Upper, Op::ConjTrans, real_t(1),  A01, real_t(1), A11);
+
+    trmm(Side::Left, Uplo::Upper, Op::ConjTrans, Diag::NonUnit, real_t(1), A00, A01);
+
+    mult_chc(A00);
+
+    return;
+}
+//---------------------------------------------------------
+// template<TLAPACK_SMATRIX matrix_t, TLAPACK_SMATRIX matrix_t>
+// void mult_ab_upper(matrixA_t& A, matrixB_t& B) 
+// {
+//     using T = type_t<matrix_t>;
+//     typedef tlapack::real_type<T> real_t;
+//     using idx_t = tlapack::size_type<matrix_t>;
+//     using range = pair<idx_t, idx_t>;
+
+//     const idx_t m = nrows(A);
+//     const idx_t n = ncols(A);
+
+//     if (m != n)
+//         return;
+
+//     if (n <= 1){
+//     B(0,0) = real(A(0,0))*real(B(0,0));
+//     return;
+//     }
+
+//     const idx_t n0 = n/2;
+
+//     auto A00 = slice(A, range(0, n0), range(0, n0));
+//     auto A01 = slice(A, range(0, n0), range(n0, n));
+//     auto A11 = slice(A, range(n0, n), range(n0, n));
+
+//     auto B00 = slice(A, range(0, n0), range(0, n0));
+//     auto B01 = slice(A, range(0, n0), range(n0, n));
+//     auto B11 = slice(A, range(n0, n), range(n0, n));
+//
+//     mult_hehe(A11, B11);
+//
+//     //A01^H * B01
+//     gemm(Op::ConjTrans, Op::NoTrans, real_t(1), A01, B01, );
+//     
+// }
+//--------------------------------------------------------------
+template <typename T>
+void run(size_t n, size_t k)
+{
+    using std::size_t;
+    
+    using matrix_t = tlapack::LegacyMatrix<T>;
+    using idx_t = tlapack::size_type<matrix_t>;
+    size_t info;
+    typedef tlapack::real_type<T> real_t;
+
+    // Functors for creating new matrices
+    tlapack::Create<matrix_t> new_matrix;
+
+
+    // Turn it off if m or n are large
+    //bool verbose = false;
+    bool verbose = true;
+
+
+    // Matrices
+    std::vector<T> A_(n * n);
+    tlapack::LegacyMatrix<T> A(n, n, &A_[0], n);
+
+    std::vector<T> b_(n * k);
+    tlapack::LegacyMatrix<T> b(n, k, &b_[0], n);
+
+    std::vector<T> C_(n * n);
+    tlapack::LegacyMatrix<T> C(n, n, &C_[0], n);
+
+    std::vector<T> y_(n * k);
+    tlapack::LegacyMatrix<T> y(n, k, &y_[0], n);
+
+    // // Initialize arrays with junk
+    for (size_t j = 0; j < n; ++j) {
+         for (size_t i = 0; i < n; ++i) {
+            if constexpr (tlapack::is_complex<T>)
+              A(i, j) = T(static_cast<float>(0xDEADBEEF),
+                        static_cast<float>(0xDEADBEEF));
+            else
+                A(i, j) = T(static_cast<float>(0xDEADBEEF));
+     }
+    } 
+
+    //Generate a random matrix in A
+    for (size_t j = 0; j < n; ++j){
+        for (size_t i = 0; i <= j; ++i) {
+            if constexpr (tlapack::is_complex<T>)
+                A(i, j) = T(static_cast<float>(rand())/static_cast<float>(RAND_MAX), static_cast<float>(rand())/static_cast<float>(RAND_MAX));
+            else
+                A(i, j) = T(static_cast<float>(rand()) / static_cast<float>(RAND_MAX));
+        }
+        A(j, j) = T(n + static_cast<float>(rand())/static_cast<float>(RAND_MAX));
+    }
+
+    for (size_t j = 0; j < k; ++j)
+        for (size_t i = 0; i < n; ++i) {
+            if constexpr (tlapack::is_complex<T>)
+                b(i, j) = T(static_cast<float>(rand())/static_cast<float>(RAND_MAX), static_cast<float>(rand())/static_cast<float>(RAND_MAX));
+            else
+                b(i, j) = T(static_cast<float>(rand()) / static_cast<float>(RAND_MAX));
+        }
+
+    if (verbose) {
+        std::cout << std::endl << "A = ";
+        printMatrix(A);
+        std::cout << std::endl;
+    }
+
+    real_t normA;
+
+    normA = tlapack::lanhe(tlapack::FROB_NORM, tlapack::Uplo::Upper, A);
+
+    tlapack::lacpy(tlapack::GENERAL, A, C);
+    tlapack::lacpy(tlapack::GENERAL, b, y);
+
+    info = potrf2(tlapack::UPPER_TRIANGLE, C);
+
+    trtri_recursive(Uplo::Upper,Diag::NonUnit, C);
+
+    lauum_recursive(Uplo::Upper, C);
+
+    std::cout << "The inverse of A is =";
+    printMatrix(C);
+    std::cout << std::endl;
+
+//-------------------------------------------------checking inverse----------------------------------------------------------
+
+    for (size_t i = 0; i < n; ++i){
+        for (size_t j = 0; j < i; ++j) {
+            A(i, j) = conj(A(j, i));
+            C(i, j) = conj(C(j, i));
+        }
+    }
+
+    std::cout << "New A =";
+    std::cout << std::endl;
+    printMatrix(A);
+    std::cout << std::endl;
+
+    std::cout << "New A inverse=";
+    std::cout << std::endl;
+    printMatrix(C);
+
+    std::vector<T> I_(n * n);
+    tlapack::LegacyMatrix<T> I(n, n, &I_[0], n);
+
+    for (size_t i = 0; i < n; ++i){
+        I(i, i) = real_t(1);
+    }
+
+    gemm(Op::NoTrans, Op::NoTrans, real_t(1), A, C, real_t(-1), I);
+
+    real_t normr;
+    normr = tlapack::lange(tlapack::FROB_NORM, I);
+
+    std::cout << std::endl;
+    std::cout << "Norm of I is= " << normr;
+
+//-------------------------------------------------checking factorization----------------------------------------------------------
+//     std::cout << std::endl;
+//     std::cout << "info = " << info;
+//     std::cout << std::endl;
+
+//     //checking factorization
+//     tlapack::trsm(tlapack::Side::Left, tlapack::Uplo::Upper, tlapack::Op::ConjTrans, tlapack::Diag::NonUnit, real_t(1), C, y);
+//     tlapack::trsm(tlapack::Side::Left, tlapack::Uplo::Upper, tlapack::Op::NoTrans, tlapack::Diag::NonUnit, real_t(1), C, y);
+
+
+//     tlapack::hemm(tlapack::Side::Left, tlapack::Uplo::Upper, real_t(-1), A, y, real_t(1), b);
+
+//     real_t normy;
+//     real_t normr;
+
+//     normr = tlapack::lange(tlapack::FROB_NORM, b);
+
+//     normy = tlapack::lange(tlapack::FROB_NORM, y);
+
+//     real_t check;
+
+//     check = normr/(normy * normA);
+
+
+//     real_t normC2;
+//     normC2 =  tlapack::lanhe(tlapack::FROB_NORM, tlapack::Uplo::Upper, C);
+
+//     normC2 *= normC2;
+
+//     mult_chc(C);
+
+//     std::cout << "check A is = ";
+//     printMatrix(C);
+//     std::cout << std::endl;
+
+//     for(idx_t i = n; i-- >0;){
+//         for(idx_t j = n; j-- > 0; ){
+//             C(i, j) -= A(i, j);
+//         }
+//     }
+
+//     real_t normC;
+//     normC =  tlapack::lanhe(tlapack::FROB_NORM, tlapack::Uplo::Upper, C);
+
+//     normC = normC/normC2;
+
+//     std::cout << "||U^H U - A||/||U||^2 = " << normC;
+//     std::cout << std::endl;
+
+ 
+
+ }
+
+
+
+//------------------------------------------------------------------------------
+int main(int argc, char** argv)
+{
+    int n, k;
+
+    // Default arguments
+    n = (argc < 2) ? 5 : atoi(argv[1]);
+    k = (argc < 3) ? 2 : atoi(argv[2]);
+
+
+    srand(3);  // Init random seed
+
+    std::cout.precision(5);
+    std::cout << std::scientific << std::showpos;
+
+     printf("run< float  >( %d, %d )", n, k);
+     run<float>(n, k);
+     printf("-----------------------\n");
+
+     printf("run< double >( %d, %d )", n, k);
+     run<double>(n, k);
+     printf("-----------------------\n");
+
+     printf("run< long double >( %d, %d )", n, k);
+     run<long double>(n, k);
+     printf("-----------------------\n");
+
+     printf("run complex< float >( %d, %d )", n, k);
+     run<std::complex<float> >(n, k);
+     printf("-----------------------\n");
+
+     printf("run complex< double >( %d, %d )", n, k);
+     run<std::complex<double> >(n, k);
+     printf("-----------------------\n");
+
+
+
+    return 0;
+}
