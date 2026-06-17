9eae0298fe687f0fa5a69570146942c0d38dfd6d
steelthijs <thijs.steel@kuleuven.be>
2026-06-17 11:28:37 +0200

    improve unit test of qz eig22 and improve the accuracy of infinite and complex eigenvalues

--- include/tlapack/lapack/lahqz_eig22.hpp
+++ include/tlapack/lapack/lahqz_eig22.hpp
@@ -64,15 +64,93 @@ void lahqz_eig22(const A_t& A,
     TA a10 = ascale * A(1, 0);
     TA a11 = ascale * A(1, 1);
     //
-    // Perturb B if necessary to ensure non-singularity
+    // If B is singular, deflate infinite eigenvalue
+    // Note, here we deviate from LAPACK, as LAPACK perturbs B
+    // to make it non-singular, but this removes the possibility
+    // for infinite eigenvalues
     //
     TA b00 = B(0, 0);
     TA b01 = B(0, 1);
     TA b11 = B(1, 1);
     real_t bmin = rtmin * std::max(std::max(abs1(b00), abs1(b01)),
                                    std::max(abs1(b11), rtmin));
-    if (abs1(b00) < bmin) b00 = bmin;  // TODO: figure out sign
-    if (abs1(b11) < bmin) b11 = bmin;  // TODO: figure out sign
+    if (abs1(b00) < bmin) {
+        b00 = zero;
+        // B(0,0) is zero, we can apply rotations to the left
+        // to make A(1,0) zero without perturbing the upper triangular
+        // structure of B
+        if (a10 != zero) {
+            real_t c;
+            TA s;
+            rotg(a00, a10, c, s);
+            a10 = zero;
+            TA temp = c * a01 + s * a11;
+            a11 = c * a11 - conj(s) * a01;
+            a01 = temp;
+            temp = c * b01 + s * b11;
+            b11 = c * b11 - conj(s) * b01;
+            b01 = temp;
+        }
+
+        // Apply scaling to the right of B to make B(1,1) real
+        if constexpr (is_complex<TA>) {
+            if (abs1(b11) < bmin) {
+                b11 = zero;
+            }
+            else {
+                TA scale = conj(b11) / abs(b11);
+                b01 *= scale;
+                b11 = abs(b11);
+                a01 *= scale;
+                a11 *= scale;
+            }
+        }
+
+        alpha1 = a00;
+        alpha2 = a11;
+        beta1 = zero;
+        beta2 = real(b11) * ascale;
+        return;
+    }
+
+    if (abs1(b11) < bmin) {
+        b11 = zero;
+        // B(1,1) is zero, we can apply rotations to the right
+        // to make A(1,0) zero without perturbing the upper triangular
+        // structure of B
+        if (a10 != zero) {
+            real_t c;
+            TA s;
+            rotg(a11, a10, c, s);
+            a10 = zero;
+            TA temp = c * a01 + s * a00;
+            a00 = c * a00 - conj(s) * a01;
+            a01 = temp;
+            temp = c * b01 + s * b00;
+            b00 = c * b00 - conj(s) * b01;
+            b01 = temp;
+        }
+
+        // Apply scaling to the right of B to make B(0,0) real
+        if constexpr (is_complex<TA>) {
+            if (abs1(b00) < bmin) {
+                b00 = zero;
+            }
+            else {
+                TA scale = conj(b00) / abs(b00);
+                b01 *= scale;
+                b00 = abs(b00);
+                a01 *= scale;
+                a00 *= scale;
+            }
+        }
+
+        alpha1 = a00;
+        alpha2 = a11;
+        beta1 = real(b00) * ascale;
+        beta2 = zero;
+        return;
+    }
     //
     // Scale B
     //
@@ -127,8 +205,18 @@ void lahqz_eig22(const A_t& A,
     }
 
     if constexpr (is_complex<TA>) {
-        alpha1 = shift + pp + sqrt(discr);
-        alpha2 = shift + pp - sqrt(discr);
+        TA root = sqrt(discr);
+
+        TA mu_big;
+        if (abs1(pp + root) > abs1(pp - root)) {
+            mu_big = pp + root;
+        }
+        else {
+            mu_big = pp - root;
+        }
+        TA mu_small = -qq / mu_big;
+        alpha1 = shift + mu_big;
+        alpha2 = shift + mu_small;
     }
     else {
         //
--- test/src/test_qz_eig22.cpp
+++ test/src/test_qz_eig22.cpp
@@ -15,13 +15,17 @@
 #include <tlapack/lapack/lange.hpp>
 
 // Other routines
+#include <tlapack/blas/rot.hpp>
+#include <tlapack/blas/rotg.hpp>
 #include <tlapack/lapack/lahqz_eig22.hpp>
 
 using namespace tlapack;
 
-TEMPLATE_TEST_CASE("check that lahqz_eig22 gives correct eigenvalues",
-                   "[generalizedeigenvalues]",
-                   TLAPACK_TYPES_TO_TEST)
+TEMPLATE_TEST_CASE(
+    "check that lahqz_eig22 gives correct eigenvalues on random 2x2 "
+    "pencils",
+    "[generalizedeigenvalues]",
+    TLAPACK_TYPES_TO_TEST)
 {
     using matrix_t = TestType;
     using T = type_t<matrix_t>;
@@ -35,55 +39,172 @@ TEMPLATE_TEST_CASE("check that lahqz_eig22 gives correct eigenvalues",
 
     // MatrixMarket reader
     uint64_t seed = GENERATE(1, 2, 3, 4, 5, 6);
-    MatrixMarket mm;
-    mm.gen.seed(seed);
-
-    const real_type<T> eps = ulp<real_type<T>>();
-
-    std::vector<T> A_;
-    auto A = new_matrix(A_, 2, 2);
-    std::vector<T> B_;
-    auto B = new_matrix(B_, 2, 2);
-    std::vector<complex_t> As_;
-    auto As = new_complex_matrix(As_, 2, 2);
-
-    mm.random(A);
-    mm.random(B);
-
-    B(1, 0) = T(0);
-
-    real_t beta1, beta2;
-    std::complex<real_t> alpha1, alpha2;
-    lahqz_eig22(A, B, alpha1, alpha2, beta1, beta2);
-
-    auto normA = lange(MAX_NORM, A);
-    auto normB = lange(MAX_NORM, B);
-
-    // Check first eigenvalue
-    {
-        for (idx_t i = 0; i < 2; ++i)
-            for (idx_t j = 0; j < 2; ++j)
-                As(i, j) = beta1 * A(i, j) - alpha1 * B(i, j);
-
-        // Note, once svd is merge, this could be replaced with svd22
-        auto normAs = lange(MAX_NORM, As);
-        auto As_scale = real_t(0) / sqrt(normAs);
-        auto det = (As_scale * As(0, 0)) * (As_scale * As(1, 1)) -
-                   (As_scale * As(1, 0)) * (As_scale * As(0, 1));
-        auto temp = max(abs(beta1) * normA, abs(alpha1) * normB);
-        CHECK(abs(det) <= 1.0e1 * eps * temp);
+    bool complex_conjugate = GENERATE(true, false);
+    bool singular0 = GENERATE(true, false);
+    bool singular1 = GENERATE(true, false);
+    if constexpr (is_complex<T>) {
+        complex_conjugate = false;
     }
 
-    // Check second eigenvalue
+    DYNAMIC_SECTION(" seed = " << seed << " complex conjugate ? "
+                               << complex_conjugate << " singular0 ? "
+                               << singular0 << " singular1 ? " << singular1)
     {
-        for (idx_t i = 0; i < 2; ++i)
-            for (idx_t j = 0; j < 2; ++j)
-                As(i, j) = beta2 * A(i, j) - alpha2 * B(i, j);
+        MatrixMarket mm;
+        mm.gen.seed(seed);
 
-        // Note, once svd is merge, this could be replaced with svd22
-        auto normAs = lange(MAX_NORM, As);
-        auto det = As(0, 0) * As(1, 1) - As(1, 0) * As(0, 1);
-        auto temp = max(abs(beta2) * normA, abs(alpha2) * normAs);
-        CHECK(abs(det) <= 1.0e1 * eps * temp);
+        const real_type<T> eps = ulp<real_type<T>>();
+
+        std::vector<T> A_;
+        auto A = new_matrix(A_, 2, 2);
+        std::vector<T> B_;
+        auto B = new_matrix(B_, 2, 2);
+
+        // Generate pencil with prescibed eigenvalues
+
+        real_t beta1_e = rand_helper<real_t>(mm.gen);
+        real_t beta2_e = rand_helper<real_t>(mm.gen);
+
+        if (singular0) {
+            beta1_e = 0;
+        }
+        if (singular1) {
+            beta2_e = 0;
+        }
+
+        complex_t alpha1_e, alpha2_e;
+        if constexpr (is_complex<T>) {
+            alpha1_e = complex_t(rand_helper<real_t>(mm.gen),
+                                 rand_helper<real_t>(mm.gen));
+            alpha2_e = complex_t(rand_helper<real_t>(mm.gen),
+                                 rand_helper<real_t>(mm.gen));
+
+            A(0, 0) = alpha1_e;
+            A(1, 0) = T(0);
+            A(0, 1) = rand_helper<T>(mm.gen);
+            A(1, 1) = alpha2_e;
+
+            B(0, 0) = beta1_e;
+            B(1, 1) = beta2_e;
+            B(0, 1) = rand_helper<T>(mm.gen);
+            B(1, 0) = T(0);
+        }
+        else {
+            if (complex_conjugate) {
+                real_t alpha1_real = rand_helper<real_t>(mm.gen);
+                real_t alpha1_imag = rand_helper<real_t>(mm.gen);
+                alpha1_e = complex_t(alpha1_real, alpha1_imag);
+                alpha2_e = complex_t(alpha1_real, -alpha1_imag);
+
+                beta2_e = beta1_e;
+
+                A(0, 0) = alpha1_real;
+                A(1, 1) = alpha1_real;
+
+                real_t temp = rand_helper<real_t>(mm.gen);
+                A(0, 1) = alpha1_imag * alpha1_imag / temp;
+                A(1, 0) = -temp;
+
+                B(0, 0) = beta1_e;
+                B(1, 1) = beta2_e;
+                B(0, 1) = T(0);
+                B(1, 0) = T(0);
+            }
+            else {
+                alpha1_e = rand_helper<real_t>(mm.gen);
+                alpha2_e = rand_helper<real_t>(mm.gen);
+
+                A(0, 0) = real(alpha1_e);
+                A(1, 0) = T(0);
+                A(0, 1) = rand_helper<T>(mm.gen);
+                A(1, 1) = real(alpha2_e);
+
+                B(0, 0) = beta1_e;
+                B(1, 1) = beta2_e;
+                B(0, 1) = rand_helper<T>(mm.gen);
+                B(1, 0) = T(0);
+            }
+        }
+
+        // Apply random orthogonal transformation (rotations) to the pencil to
+        // make it more general
+        {
+            T x = rand_helper<T>(mm.gen);
+            T y = rand_helper<T>(mm.gen);
+            real_t c;
+            T s;
+            rotg(x, y, c, s);
+
+            auto a1 = col(A, 0);
+            auto a2 = col(A, 1);
+            rot(a1, a2, c, s);
+
+            auto b1 = col(B, 0);
+            auto b2 = col(B, 1);
+            rot(b1, b2, c, s);
+        }
+        {
+            T x = B(0, 0);
+            T y = B(1, 0);
+            real_t c;
+            T s;
+            rotg(x, y, c, s);
+
+            auto a1 = row(A, 0);
+            auto a2 = row(A, 1);
+            rot(a1, a2, c, s);
+
+            auto b1 = row(B, 0);
+            auto b2 = row(B, 1);
+            rot(b1, b2, c, s);
+
+            B(1, 0) = 0;
+        }
+
+        real_t beta1, beta2;
+        std::complex<real_t> alpha1, alpha2;
+        lahqz_eig22(A, B, alpha1, alpha2, beta1, beta2);
+
+        // Check that the computed eigenvalues are close to the expected
+        // eigenvalues
+        // We need to take into account that the eigenvalues may not be in the
+        // same order, so we check both possibilities after normalizing
+
+        real_t s1 = sqrt(abs(alpha1) * abs(alpha1) + abs(beta1) * abs(beta1));
+        real_t s2 = sqrt(abs(alpha2) * abs(alpha2) + abs(beta2) * abs(beta2));
+        real_t s1_e =
+            sqrt(abs(alpha1_e) * abs(alpha1_e) + abs(beta1_e) * abs(beta1_e));
+        real_t s2_e =
+            sqrt(abs(alpha2_e) * abs(alpha2_e) + abs(beta2_e) * abs(beta2_e));
+
+        complex_t alpha1_n = alpha1 / s1;
+        real_t beta1_n = beta1 / s1;
+        complex_t alpha2_n = alpha2 / s2;
+        real_t beta2_n = beta2 / s2;
+        complex_t alpha1_e_n = alpha1_e / s1_e;
+        real_t beta1_e_n = beta1_e / s1_e;
+        complex_t alpha2_e_n = alpha2_e / s2_e;
+        real_t beta2_e_n = beta2_e / s2_e;
+
+        // Error where we compare alpha1 with alpha1_e and alpha2 with alpha2_e
+        real_t err11 = 2 - 2 * abs(alpha1_n * conj(alpha1_e_n) +
+                                   beta1_n * conj(beta1_e_n));
+        real_t err12 = 2 - 2 * abs(alpha2_n * conj(alpha2_e_n) +
+                                   beta2_n * conj(beta2_e_n));
+
+        // Error where we compare alpha1 with alpha2_e and alpha2 with alpha1_e
+        real_t err21 = 2 - 2 * abs(alpha1_n * conj(alpha2_e_n) +
+                                   beta1_n * conj(beta2_e_n));
+        real_t err22 = 2 - 2 * abs(alpha2_n * conj(alpha1_e_n) +
+                                   beta2_n * conj(beta1_e_n));
+
+        if (err11 < err21) {
+            CHECK(err11 < 10 * eps);
+            CHECK(err12 < 10 * eps);
+        }
+        else {
+            CHECK(err21 < 10 * eps);
+            CHECK(err22 < 10 * eps);
+        }
     }
 }
