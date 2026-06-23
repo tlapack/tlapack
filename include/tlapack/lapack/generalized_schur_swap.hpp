2de941a43f0147d0683c7fc72c9c48098edba549
steelthijs <thijs.steel@kuleuven.be>
2026-06-23 12:56:02 +0200

    implement iterative refinement

--- include/tlapack/lapack/generalized_schur_swap.hpp
+++ include/tlapack/lapack/generalized_schur_swap.hpp
@@ -581,7 +581,7 @@ int generalized_schur_swap(bool want_q,
 
         // Find Zc so that
         //       [ -x[0] -x[2] ]   [ *  * ]
-        //  Zc^T  [ -x[1] -x[3] ] = [ *  * ]
+        //  Zc^T [ -x[1] -x[3] ] = [ *  * ]
         //       [ 1     0     ]   [ 0  0 ]
         //       [ 0     1     ]   [ 0  0 ]
 
@@ -640,9 +640,15 @@ int generalized_schur_swap(bool want_q,
         auto AA = new_matrix(AA_, 4, 4);
         std::vector<T> BB_;
         auto BB = new_matrix(BB_, 4, 4);
+        std::vector<T> QQ_;
+        auto QQ = new_matrix(QQ_, 4, 4);
+        std::vector<T> ZZ_;
+        auto ZZ = new_matrix(ZZ_, 4, 4);
 
         lacpy(GENERAL, slice(A, range(j0, j3 + 1), range(j0, j3 + 1)), AA);
         lacpy(GENERAL, slice(B, range(j0, j3 + 1), range(j0, j3 + 1)), BB);
+        laset(GENERAL, T(0), T(1), QQ);
+        laset(GENERAL, T(0), T(1), ZZ);
 
         auto norma = lange(FROB_NORM, AA);
         auto normb = lange(FROB_NORM, BB);
@@ -666,6 +672,16 @@ int generalized_schur_swap(bool want_q,
             rot(b2, b3, cyl, syl);
             rot(b2, b0, cy1, sy1);
             rot(b3, b1, cy2, sy2);
+
+            auto q0 = col(QQ, 0);
+            auto q1 = col(QQ, 1);
+            auto q2 = col(QQ, 2);
+            auto q3 = col(QQ, 3);
+
+            rot(q0, q1, cyr, syr);
+            rot(q2, q3, cyl, syl);
+            rot(q2, q0, cy1, sy1);
+            rot(q3, q1, cy2, sy2);
         }
         // Apply rotations from the right to local matrices
         {
@@ -686,6 +702,15 @@ int generalized_schur_swap(bool want_q,
             rot(b2, b3, cxr, sxr);
             rot(b0, b2, cx1, sx1);
             rot(b1, b3, cx2, sx2);
+
+            auto z0 = col(ZZ, 0);
+            auto z1 = col(ZZ, 1);
+            auto z2 = col(ZZ, 2);
+            auto z3 = col(ZZ, 3);
+            rot(z0, z1, cxl, sxl);
+            rot(z2, z3, cxr, sxr);
+            rot(z0, z2, cx1, sx1);
+            rot(z1, z3, cx2, sx2);
         }
 
         // Weak stability test
@@ -693,68 +718,208 @@ int generalized_schur_swap(bool want_q,
         auto enormb = lange(FROB_NORM, slice(BB, range(2, 4), range(0, 2)));
         const T eps = ulp<T>();
         const T small_num = safe_min<T>();
-        if (enorma > max((T)20 * norma * eps, small_num)) return 1;
-        if (enormb > max((T)20 * normb * eps, small_num)) return 1;
 
-        // Apply rotations from the left
-        {
-            auto a0 = slice(A, j0, range(j0, n));
-            auto a1 = slice(A, j1, range(j0, n));
-            auto a2 = slice(A, j2, range(j0, n));
-            auto a3 = slice(A, j3, range(j0, n));
-            rot(a0, a1, cyr, syr);
-            rot(a2, a3, cyl, syl);
-            rot(a2, a0, cy1, sy1);
-            rot(a3, a1, cy2, sy2);
+        idx_t iter = 0;
+        T tolA = max((T)20 * norma * eps, small_num);
+        T tolB = max((T)20 * normb * eps, small_num);
+        while (iter < 30) {
+            std::cout << "enorma: " << enorma << " tolA: " << tolA
+                      << " enormb: " << enormb << " tolB: " << tolB
+                      << std::endl;
+            if (enorma <= tolA and enormb <= tolB) break;
+            if (iter == 29) return 1;
+            // The swap is not (yet) accepted, apply iterative refinement to
+            // try to improve the swap.
 
-            auto b0 = slice(B, j0, range(j0, n));
-            auto b1 = slice(B, j1, range(j0, n));
-            auto b2 = slice(B, j2, range(j0, n));
-            auto b3 = slice(B, j3, range(j0, n));
-            rot(b0, b1, cyr, syr);
-            rot(b2, b3, cyl, syl);
-            rot(b2, b0, cy1, sy1);
-            rot(b3, b1, cy2, sy2);
+            for (idx_t j = 0; j < 8; ++j)
+                for (idx_t i = 0; i < 8; ++i)
+                    M(i, j) = (T)0;
 
-            auto q0 = col(Q, j0);
-            auto q1 = col(Q, j1);
-            auto q2 = col(Q, j2);
-            auto q3 = col(Q, j3);
-            rot(q0, q1, cyr, syr);
-            rot(q2, q3, cyl, syl);
-            rot(q2, q0, cy1, sy1);
-            rot(q3, q1, cy2, sy2);
+            /// Construct matrix with kronecker structure
+            // I (x) AA(2:3,2:3)
+            M(0, 0) = AA(2, 2);
+            M(0, 1) = AA(2, 3);
+            M(1, 0) = AA(3, 2);
+            M(1, 1) = AA(3, 3);
+            M(2, 2) = AA(2, 2);
+            M(2, 3) = AA(2, 3);
+            M(3, 2) = AA(3, 2);
+            M(3, 3) = AA(3, 3);
+            // -AA(0:1,0:1)^T (x) I
+            M(0, 4) = -AA(0, 0);
+            M(0, 5) = -AA(1, 0);
+            M(1, 6) = -AA(0, 0);
+            M(1, 7) = -AA(1, 0);
+            M(2, 4) = -AA(0, 1);
+            M(2, 5) = -AA(1, 1);
+            M(3, 6) = -AA(0, 1);
+            M(3, 7) = -AA(1, 1);
+
+            // I (x) BB(2:3,2:3)
+            M(4, 0) = BB(2, 2);
+            M(4, 1) = BB(2, 3);
+            M(5, 0) = BB(3, 2);
+            M(5, 1) = BB(3, 3);
+            M(6, 2) = BB(2, 2);
+            M(6, 3) = BB(2, 3);
+            M(7, 2) = BB(3, 2);
+            M(7, 3) = BB(3, 3);
+            // -BB(0:1,0:1)^T (x) I
+            M(4, 4) = -BB(0, 0);
+            M(4, 5) = -BB(1, 0);
+            M(5, 6) = -BB(0, 0);
+            M(5, 7) = -BB(1, 0);
+            M(6, 4) = -BB(0, 1);
+            M(6, 5) = -BB(1, 1);
+            M(7, 6) = -BB(0, 1);
+            M(7, 7) = -BB(1, 1);
+
+            // RHS
+            x[0] = AA(2, 0);
+            x[1] = AA(3, 0);
+            x[2] = AA(2, 1);
+            x[3] = AA(3, 1);
+            x[4] = BB(2, 0);
+            x[5] = BB(3, 0);
+            x[6] = BB(2, 1);
+            x[7] = BB(3, 1);
+
+            // LU of M
+            int ierr = getrf(M, piv);
+            if (ierr != 0) return 1;
+            // Apply pivot to rhs
+            for (idx_t i = 0; i < 8; ++i) {
+                if (i != piv[i]) std::swap(x[i], x[piv[i]]);
+            }
+            // Solve Ly = rhs
+            trsv(LOWER_TRIANGLE, NO_TRANS, UNIT_DIAG, M, x);
+            // Solve Ux = y
+            trsv(UPPER_TRIANGLE, NO_TRANS, NON_UNIT_DIAG, M, x);
+
+            // Find Zc so that
+            //       [ 1      0    ]   [ *  * ]
+            //  Zc^T [ 0      1    ] = [ *  * ]
+            //       [ -x[0] -x[2] ]   [ 0  0 ]
+            //       [ -x[1] -x[3] ]   [ 0  0 ]
+
+            // Rotation to make X upper triangular
+            T cxl1, sxl1;
+            rotg(x[0], x[1], cxl1, sxl1);
+            x[1] = (T)0;
+            T rottemp = cxl1 * x[2] + sxl1 * x[3];
+            x[3] = -sxl1 * x[2] + cxl1 * x[3];
+            x[2] = rottemp;
+            // SVD of (upper triangular) X
+            T cxl2, sxl2, cxr, sxr, ssx1, ssx2;
+            svd22(x[0], x[2], x[3], ssx2, ssx1, cxl2, sxl2, cxr, sxr);
+            // Fuse left rotations
+            T cxl, sxl;
+            cxl = cxl1 * cxl2 - sxl1 * sxl2;
+            sxl = cxl2 * sxl1 + sxl2 * cxl1;
+            // Rotations based on the singular values
+            ssx1 = -ssx1;
+            ssx2 = -ssx2;
+            T temp = (T)1;
+            T cx1, sx1, cx2, sx2;
+            rotg(temp, ssx1, cx1, sx1);
+            temp = (T)1;
+            rotg(temp, ssx2, cx2, sx2);
+
+            // Find Qc so that
+            //       [ 1     0     ]   [ 0  0 ]
+            //  Qc^T  [ 0     1     ] = [ 0  0 ]
+            //       [ x[4]  x[6]  ]   [ *  * ]
+            //       [ x[5]  x[7]  ]   [ *  * ]
+
+            // Rotation to make Y^T upper triangular
+            T cyl1, syl1;
+            rotg(x[4], x[5], cyl1, syl1);
+            x[5] = (T)0;
+            rottemp = cyl1 * x[6] + syl1 * x[7];
+            x[7] = -syl1 * x[6] + cyl1 * x[7];
+            x[6] = rottemp;
+            // SVD of (upper triangular) Y
+            T cyl2, syl2, cyr, syr, ssy1, ssy2;
+            svd22(x[4], x[6], x[7], ssy2, ssy1, cyl2, syl2, cyr, syr);
+            // Fuse left rotations
+            T cyl, syl;
+            cyl = cyl1 * cyl2 - syl1 * syl2;
+            syl = cyl2 * syl1 + syl2 * cyl1;
+            // Rotations based on the singular values
+            temp = (T)1;
+            ssx1 = -ssx1;
+            ssx2 = -ssx2;
+            T cy1, sy1, cy2, sy2;
+            rotg(temp, ssy1, cy1, sy1);
+            temp = (T)1;
+            rotg(temp, ssy2, cy2, sy2);
+
+            // Apply rotations from the left to local matrices
+            {
+                auto a0 = row(AA, 0);
+                auto a1 = row(AA, 1);
+                auto a2 = row(AA, 2);
+                auto a3 = row(AA, 3);
+                rot(a0, a1, cyr, syr);
+                rot(a2, a3, cyl, syl);
+                rot(a0, a2, cy1, -sy1);
+                rot(a1, a3, cy2, -sy2);
+
+                auto b0 = row(BB, 0);
+                auto b1 = row(BB, 1);
+                auto b2 = row(BB, 2);
+                auto b3 = row(BB, 3);
+                rot(b0, b1, cyr, syr);
+                rot(b2, b3, cyl, syl);
+                rot(b0, b2, cy1, -sy1);
+                rot(b1, b3, cy2, -sy2);
+
+                auto q0 = col(QQ, 0);
+                auto q1 = col(QQ, 1);
+                auto q2 = col(QQ, 2);
+                auto q3 = col(QQ, 3);
+
+                rot(q0, q1, cyr, syr);
+                rot(q2, q3, cyl, syl);
+                rot(q0, q2, cy1, -sy1);
+                rot(q1, q3, cy2, -sy2);
+            }
+            // Apply rotations from the right to local matrices
+            {
+                auto a0 = col(AA, 0);
+                auto a1 = col(AA, 1);
+                auto a2 = col(AA, 2);
+                auto a3 = col(AA, 3);
+                rot(a0, a1, cxr, sxr);
+                rot(a2, a3, cxl, sxl);
+                rot(a0, a2, cx1, sx1);
+                rot(a1, a3, cx2, sx2);
+
+                auto b0 = col(BB, 0);
+                auto b1 = col(BB, 1);
+                auto b2 = col(BB, 2);
+                auto b3 = col(BB, 3);
+                rot(b0, b1, cxr, sxr);
+                rot(b2, b3, cxl, sxl);
+                rot(b0, b2, cx1, sx1);
+                rot(b1, b3, cx2, sx2);
+
+                auto z0 = col(ZZ, 0);
+                auto z1 = col(ZZ, 1);
+                auto z2 = col(ZZ, 2);
+                auto z3 = col(ZZ, 3);
+                rot(z0, z1, cxr, sxr);
+                rot(z2, z3, cxl, sxl);
+                rot(z0, z2, cx1, sx1);
+                rot(z1, z3, cx2, sx2);
+            }
+
+            enorma = lange(FROB_NORM, slice(AA, range(2, 4), range(0, 2)));
+            enormb = lange(FROB_NORM, slice(BB, range(2, 4), range(0, 2)));
+            iter++;
         }
 
-        // Apply rotations from the right
-        {
-            auto a0 = slice(A, range(0, j3 + 1), j0);
-            auto a1 = slice(A, range(0, j3 + 1), j1);
-            auto a2 = slice(A, range(0, j3 + 1), j2);
-            auto a3 = slice(A, range(0, j3 + 1), j3);
-            rot(a0, a1, cxl, sxl);
-            rot(a2, a3, cxr, sxr);
-            rot(a0, a2, cx1, sx1);
-            rot(a1, a3, cx2, sx2);
-
-            auto b0 = slice(B, range(0, j3 + 1), j0);
-            auto b1 = slice(B, range(0, j3 + 1), j1);
-            auto b2 = slice(B, range(0, j3 + 1), j2);
-            auto b3 = slice(B, range(0, j3 + 1), j3);
-            rot(b0, b1, cxl, sxl);
-            rot(b2, b3, cxr, sxr);
-            rot(b0, b2, cx1, sx1);
-            rot(b1, b3, cx2, sx2);
-
-            auto z0 = col(Z, j0);
-            auto z1 = col(Z, j1);
-            auto z2 = col(Z, j2);
-            auto z3 = col(Z, j3);
-            rot(z0, z1, cxl, sxl);
-            rot(z2, z3, cxr, sxr);
-            rot(z0, z2, cx1, sx1);
-            rot(z1, z3, cx2, sx2);
-        }
+        // TODO: apply accumulated rotations to the original matrices
 
         // Set relevant parts to zero
         A(j2, j0) = (T)0;
